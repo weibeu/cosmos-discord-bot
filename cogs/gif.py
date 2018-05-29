d2e9bc2100c43275e4ff6aa88df1f9c0ca56a817
deepak raj <deepakrajko14@gmail.com>
2018-05-29 09:09:38 +0530

    Added with typing in gif

--- cogs/gif.py
+++ cogs/gif.py
@@ -17,17 +17,20 @@ class Gif(object):
             embed.set_image(url=util.get_random_gif())
             embed.color = int("0x"+util.get_random_color(), 16)
             embed.set_footer(text="Powered by GIPHY | Random Gif", icon_url="https://i.imgur.com/x2jgxuS.gif")
-            await ctx.send(embed=embed)
+            async with ctx.typing():
+                await ctx.send(embed=embed)
         elif query.lower() in ["-trending", "-trend", "-t"]:
             embed.set_image(url=util.get_trending_gif())
             embed.color = int("0x"+util.get_random_color(), 16)
             embed.set_footer(text="Powered by GIPHY | Trending Gif", icon_url="https://i.imgur.com/x2jgxuS.gif")
-            await ctx.send(embed=embed)
+            async with ctx.typing():
+                await ctx.send(embed=embed)
         else:
             embed.set_image(url=util.get_gif(query))
             embed.color = int("0x"+util.get_random_color(), 16)
             embed.set_footer(text="Powered by GIPHY | {0}".format(query), icon_url="https://i.imgur.com/x2jgxuS.gif")
-            await ctx.send(embed=embed)
+            async with ctx.typing():
+                await ctx.send(embed=embed)
 
 
 
