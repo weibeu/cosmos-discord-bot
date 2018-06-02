f3c0ede3d4e5c035863be8b40846e5d00b755a7d
deepak raj <deepakrajko14@gmail.com>
2018-06-02 20:35:08 +0530

    Lyrics search improvements

--- cogs/music.py
+++ cogs/music.py
@@ -1031,11 +1031,9 @@ class Music:
     @commands.cooldown(10, 30, commands.BucketType.user)
     async def get_lyrics(self, ctx, *, query=None):
         """Get lyrics of provided song or song you're listening on spotify or in voice channel.\nIf no query is provided then it tries to search lyrics of now playing song on spotify or in voice channel\n**Note:** For getting lyrics of spotify song you listening to, you must have spotify integrated with your discord account."""
-        artist_name = None
         if query is None:
             if ctx.author.activity.type.value == 2:
-                query = ctx.author.activity.title
-                artist_name = ctx.author.activity.artist.split(";")[0]
+                query = ctx.author.activity.title+" "+ctx.author.activity.artist.split(";")[0]
             else:
                 player = self.get_player(ctx)
                 if ctx.guild.voice_client is None or not ctx.guild.voice_client.is_playing():
@@ -1045,10 +1043,7 @@ class Music:
                     query = player.entry.title
 
         await ctx.trigger_typing()
-        if artist_name is None:
-            genius = Genius(ctx, query)
-        else:
-            genius = Genius(ctx, query, artist_name=artist_name)
+        genius = Genius(ctx, query)
         await genius.show_song_lyrics()
 
 
--- cogs/utils/genius.py
+++ cogs/utils/genius.py
@@ -8,18 +8,19 @@ _CAT = util.get_config()["GENIUS_CLIENT_ACCESS_TOKEN"]
 class Genius(object):
     """Fetch lyrics, artists and song info from Genius."""
 
-    def __init__(self, ctx, query, artist_name=None):
+    def __init__(self, ctx, query):
         self.client = genius.Genius(_CAT)
         self.ctx = ctx
         self.query = query
-        if artist_name is None:
-            self.song = self.client.search_song(self.query)
-        else:
-            self.song = self.client.search_song(self.query, artist_name=artist_name)
+        self.song = self.client.search_song(self.query)
         self.page = None
 
     async def show_song_lyrics(self):
-        self.page = Pages(self.ctx, entries=self.song.lyrics.split("\n"), show_entry_count=False, timeout=300, show_author=False, per_page=27)
+        try:
+            self.page = Pages(self.ctx, entries=self.song.lyrics.split("\n"), show_entry_count=False, timeout=300, show_author=False, per_page=27)
+        except:
+            await self.ctx.send("Lyrics not found ;-;")
+            return
         self.page.embed.title = f"{self.song.title} - Lyrics"
         self.page.embed.set_author(name=self.song.artist, icon_url=self.song.song_art_image_url)
         await self.page.paginate()
