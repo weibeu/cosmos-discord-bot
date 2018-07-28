24d82d0ab4ee164f4852e43aa000a874261212f5
□ | The Cosmos <deepakrajko14@gmail.com>
2018-07-28 23:37:23 +0530

    TAD LOGS

--- cogs/tad.py
+++ cogs/tad.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import datetime
 from cogs.utils.util import get_reaction_yes_no, get_random_embed_color
 
 class TAD(object):
@@ -7,12 +8,23 @@ class TAD(object):
 
     def __init__(self, bot):
         self.bot = bot
-        #self.rules = self.bot.get_channel(452702379293540372)
-        #self.server_guide = self.bot.get_channel(269610314578788363)
+        self.guild = None
+        self.invites = []
+        self.bot.loop.create_task(self.get_guild_data())
+        # self.rules = self.bot.get_channel(452702379293540372)
+        # self.server_guide = self.bot.get_channel(269610314578788363)
 
 
 
-        #bot.loop.create_task(self.rotate())
+        # bot.loop.create_task(self.rotate())
+
+    async def get_guild_data(self):
+        await self.bot.wait_until_ready()
+        self.guild = self.bot.get_guild(244998983112458240)
+        await self.get_invites()
+
+    async def get_invites(self):
+        self.invites = await self.guild.invites()
 
     '''async def rotate(self):
         await self.bot.wait_until_ready()
@@ -29,6 +41,40 @@ class TAD(object):
         '''if message.author.id == 280883146872979456 and message.attachments != []:
             await message.delete()
             await message.channel.send("lol steve fuck off")'''
+
+    async def on_member_join(self, member):
+        invite = None
+        invites = await self.guild.invites()
+        for i in invites:
+            for old_invite in self.invites:
+                if i.uses > old_invite.uses and i.id == old_invite.id:
+                    invite = i
+        if not invite:
+            async for entry in member.guild.audit_logs():
+                if entry.action == discord.AuditLogAction.invite_create:
+                    invite = entry.target
+                    break
+
+        await self.get_invites()
+
+        embed = discord.Embed(title="Member Joined", color=get_random_embed_color(), timestamp=member.joined_at)
+        embed.add_field(name="Member", value=f"{member.mention} | {member}\n**ID:** `{member.id}`")
+        embed.add_field(name="Joined at", value=f"{member.joined_at.strftime('%d - %B - %Y | %H : %M (GMT)')}")
+        embed.add_field(name="Created at", value=f"{member.created_at.strftime('%d - %B - %Y')}")
+        if invite is not None:
+            embed.add_field(name="Invite", value=f"**URL:** {invite.url}\n**Inviter:** {invite.inviter.mention} | {invite.inviter}\n**Uses:** {invite.uses}\n**Created:** {invite.created_at.strftime('%d - %B - %Y | %H : %M (GMT)')}", inline=False)
+        embed.set_thumbnail(url=member.avatar_url)
+        log_channel = self.guild.get_channel(249313961458008065)
+        await log_channel.send(embed=embed)
+
+    async def on_member_remove(self, member):
+        embed = discord.Embed(title="Member Left", color=int("0xF44336", 16))
+        embed.add_field(name="Member", value=f"{member} | {member}\n**ID:** `{member.id}`")
+        embed.add_field(name="Joined at", value=f"{member.joined_at.strftime('%d - %B - %Y | %H : %M (GMT)')}")
+        embed.add_field(name="Created at", value=f"{member.created_at.strftime('%d - %B - %Y')}")
+        embed.set_thumbnail(url=member.avatar_url)
+        log_channel = self.guild.get_channel(249313961458008065)
+        await log_channel.send(embed=embed)
 
     @commands.group(hidden=True)
     async def study(self, ctx):
