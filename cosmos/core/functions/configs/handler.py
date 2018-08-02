0e5011b280ac427031ae4b36823e5f5fb285bf34
□ | The Cosmos <deepakrajko14@gmail.com>
2018-08-02 23:38:15 +0530

    Clean Database config

--- cosmos/core/functions/configs/handler.py
+++ cosmos/core/functions/configs/handler.py
@@ -9,13 +9,14 @@ class ConfigHandler(object):
         self.discord = None
         self.cosmos = None
         self.plugins = None
-        self._get_discord_config()
-        self._get_cosmos_config()
-        self._get_plugins_config()
-        self._get_logger_config()
+        self.logger = None
+        self.db = None
+        self._get_all()
 
     @staticmethod
     def load(config_class, path):
+        if not path.startswith("cfg/"):
+            path = f"cfg/{path}"
         try:
             with open(path) as config_file:
                 config = json.load(config_file)
@@ -24,14 +25,24 @@ class ConfigHandler(object):
             print(f"Unable to find '{path}.")
             raise IOError
 
+    def _get_all(self):
+        self._get_discord_config()
+        self._get_cosmos_config()
+        self._get_plugins_config()
+        self._get_logger_config()
+        self._get_database_config()
+
     def _get_discord_config(self):
-        self.discord = self.load(DiscordConfig, "cfg/discord.json")
+        self.discord = self.load(DiscordConfig, "discord.json")
 
     def _get_cosmos_config(self):
-        self.cosmos = self.load(CosmosConfig, "cfg/cosmos.json")
+        self.cosmos = self.load(CosmosConfig, "cosmos.json")
 
     def _get_plugins_config(self):
-        self.plugins = self.load(PluginsConfig, "cfg/plugins.json")
+        self.plugins = self.load(PluginsConfig, "plugins.json")
 
     def _get_logger_config(self):
-        self.logger = self.load(LoggerConfig, "cfg/logger.json")
+        self.logger = self.load(LoggerConfig, "logger.json")
+
+    def _get_database_config(self):
+        self.db = self.load(DatabaseConfig, "database.json")
