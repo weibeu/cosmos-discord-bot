6826742a5c25650f24ced4a105182c2e37efb262
□ | The Cosmos <deepakrajko14@gmail.com>
2018-08-04 22:47:16 +0530

    Added exception for invalid sentry DSN

--- cosmos/core/functions/exceptions/handler.py
+++ cosmos/core/functions/exceptions/handler.py
@@ -1,8 +1,12 @@
 from raven import Client
+from raven.exceptions import InvalidDsn
 
 
 class ExceptionHandler(Client):
 
     def __init__(self, bot):
         self.bot = bot
-        super().__init__(self.bot.configs.sentry.dsn)
+        try:
+            super().__init__(self.bot.configs.sentry.dsn)
+        except InvalidDsn:
+            print("Invalid sentry DSN provided.")
