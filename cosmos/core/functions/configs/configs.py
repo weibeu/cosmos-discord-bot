d1188001b301a8d5d72c2284ed31adfd20fd3147
□ | The Cosmos <deepakrajko14@gmail.com>
2018-08-04 20:35:06 +0530

    Added connection to local db server without auth

--- cosmos/core/functions/configs/configs.py
+++ cosmos/core/functions/configs/configs.py
@@ -35,6 +35,7 @@ class LoggerConfig(object):
 class DatabaseConfig(object):
 
     def __init__(self, database_config):
+        self.requires_auth = None
         self.username = None
         self.password = None
         self.host = None
@@ -42,13 +43,19 @@ class DatabaseConfig(object):
         self.uri = None
         self.raw = database_config
         for config in self.raw:
+            if self.raw[config] == "":
+                self.raw[config] = None
             self.__setattr__(config, self.raw[config])
-        if "" not in [self.username, self.password, self.host, self.port]:
+        if not self.requires_auth:
+            self.uri = f"mongodb://{self.host}:{self.port}/"
+            self.__delattr__("username")
+            self.__delattr__("password")
+        elif None not in [self.username, self.password, self.host, self.port]:
             self.uri = f"mongodb://{self.username}:{self.password}@{self.host}:{self.port}/"
         else:
-            self.__delattr__('username')
-            self.__delattr__('password')
-            self.__delattr__('host')
-            self.__delattr__('port')
+            self.__delattr__("username")
+            self.__delattr__("password")
+            self.__delattr__("host")
+            self.__delattr__("port")
             if self.uri == "":
                 raise FatalError("No valid credentials found to connect to the database.")
