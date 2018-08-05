d90f8e94235c36ff40145346fc2c39d45df1f9cd
□ | The Cosmos <deepakrajko14@gmail.com>
2018-08-05 20:21:06 +0530

    Added log info in main.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from cosmos.core.cosmos import Cosmos
 
 if __name__ == "__main__":
     bot = Cosmos()
-    print(f"All initial tasks completed. [{bot.time.round_time()} seconds.]")
+    bot.log.info(f"All initial tasks completed. [{bot.time.round_time()} seconds.]")
     bot.run()
