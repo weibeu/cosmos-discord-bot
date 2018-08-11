fa168a610379f4e6f0457c5c04d8669132792846
□ | The Cosmos <deepakrajko14@gmail.com>
2018-08-11 13:52:48 +0530

    Presence edits

--- cosmos/core/plugins/presence/presence.py
+++ cosmos/core/plugins/presence/presence.py
@@ -1,4 +1,13 @@
+import discord
+
+
 class Presence(object):
 
     def __init__(self, bot):
         self.bot = bot
+
+    async def set_presence(self, activity):
+        try:
+            await self.bot.change_presence(activity=activity)
+        except discord.errors.InvalidArgument:
+            return
