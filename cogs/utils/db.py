25de9fe0a961dc98b7f71d7d1fe0e9a02bb857b2
deepakrajko14 <deepakrajko14@gmail.com>
2018-08-18 14:10:11 +0000

    Updated db.py

--- cogs/utils/db.py
+++ cogs/utils/db.py
@@ -259,7 +259,8 @@ async def get_all_points(guild_id):
     p.pop("_id")
     for user_id in p:
         try:
-            points[p[user_id]["points"]] = user_id
+            a = int(p[user_id]["points"])
+            points[a] = user_id
         except:
             pass
     return points
