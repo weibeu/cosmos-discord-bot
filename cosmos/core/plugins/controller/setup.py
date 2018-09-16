f1f5d5f34bc794c82072f50b11269640d442ff05
deepakrajko14 <deepakrajko14@gmail.com>
2018-09-16 18:17:37 +0000

    Updated setup.py

--- cosmos/core/plugins/controller/setup.py
+++ cosmos/core/plugins/controller/setup.py
@@ -1,4 +1,4 @@
-from cosmos.core.plugins.controller.controller impoer Controller
+from cosmos.core.plugins.controller.controller import Controller
 
 def setup(bot):
     bot.add_cog(Controller(bot))
