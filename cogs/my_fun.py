8cea170d30a41bc10be3ad12088097689c40fe6c
root <root@ip-172-31-85-54.ec2.internal>
2018-10-02 17:00:08 +0000

    fix

--- cogs/my_fun.py
+++ cogs/my_fun.py
@@ -3,13 +3,13 @@ class MyFun(object):
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_message(message):
+    async def on_message(self, message):
         if "shibu" in message.content.lower() and ("cosmos" in message.content.lower() or "cosmo" in message.content.lower()):
             await message.add_reaction('😡')
             await asyncio.sleep(7)
             await message.delete()
 
-    async def on_member_update(old, new):
+    async def on_member_update(self, old, new):
         if old.nick != new.nick:
             if "shibu" in new.nick.lower() and ("cosmos" in new.nick.lower() or "cosmo" in new.nick.lower()):
                 await member.edit(nick="😡 | Gross")
