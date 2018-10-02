af9525373a7079413fea44c1446e073d0aea6e4f
deepakrajko14 <deepakrajko14@gmail.com>
2018-10-02 16:10:34 +0000

    Added file my_fun.py

---
+++ cogs/my_fun.py
@@ -0,0 +1,18 @@
+class MyFun(object):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(message):
+        if "shibu" in message.content.lower() and ("cosmos" in message.content.lower() or "cosmo" in message.content.lower()):
+            await message.add_reaction('😡')
+            await asyncio.sleep(7)
+            await message.delete()
+
+    async def on_member_update(old, new):
+        if old.nick != new.nick:
+            if "shibu" in new.nick.lower() and ("cosmos" in new.nick.lower() or "cosmo" in new.nick.lower()):
+                await member.edit(nick="😡 | Gross")
+
+def setup(bot):
+    bot.add_cog(MyFun(bot))
