e12374760f862d0dc8a170ef3ac4423e9e160ce0
deepakrajko14 <deepakrajko14@gmail.com>
2018-10-03 13:53:50 +0000

    Updated my_fun.py

--- cogs/my_fun.py
+++ cogs/my_fun.py
@@ -6,12 +6,12 @@ class MyFun(object):
         self.bot = bot
 
     async def on_message(self, message):
-        if ("shibu" in message.content.lower() or "bu" in message.content.lower()) and ("cosmos" in message.content.lower() or "cosmo" in message.content.lower()):
+        if ("shibu" in message.content.lower() or "bu " in message.content.lower()) and ("cosmos" in message.content.lower() or "cosmo" in message.content.lower()):
             await message.add_reaction('😡')
 
     async def on_member_update(self, old, new):
         if old.nick != new.nick:
-            if ("shibu" in new.nick.lower() or "bu" in new.nick.lower()) and ("cosmos" in new.nick.lower() or "cosmo" in new.nick.lower()):
+            if ("shibu" in new.nick.lower() or "bu " in new.nick.lower()) and ("cosmos" in new.nick.lower() or "cosmo" in new.nick.lower()):
                 await new.edit(nick="😡 | Gross")
 
 
