ba08f8ad95408fe8e1223c879170b3c5e57c59fe
deepakrajko14 <deepakrajko14@gmail.com>
2018-10-03 14:05:21 +0000

    don't initialise sentry here

--- cosmos/core/functions/exceptions/handler.py
+++ cosmos/core/functions/exceptions/handler.py
@@ -6,7 +6,3 @@ class ExceptionHandler(object):
     def __init__(self, bot):
         self.bot = bot
         self.sentry = sentry
-        try:
-            self.sentry.init(self.bot.configs.sentry.dsn)
-        except self.utils.BadDsn:
-            self.bot.log.error("Invalid sentry DSN provided.")
