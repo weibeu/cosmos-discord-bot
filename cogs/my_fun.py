2282aae3c5dcee9c4b9a100513db8b9ca5d924dc
root <root@ip-172-31-85-54.ec2.internal>
2018-10-03 02:49:17 +0000

    #

--- cogs/my_fun.py
+++ cogs/my_fun.py
@@ -8,13 +8,12 @@ class MyFun(object):
     async def on_message(self, message):
         if ("shibu" in message.content.lower() or "bu" in message.content.lower()) and ("cosmos" in message.content.lower() or "cosmo" in message.content.lower()):
             await message.add_reaction('😡')
-            await asyncio.sleep(7)
-            await message.delete()
 
     async def on_member_update(self, old, new):
         if old.nick != new.nick:
-            if ("shibu" in message.content.lower() or "bu" in message.content.lower()) and ("cosmos" in message.content.lower() or "cosmo" in message.content.lower()):
+            if ("shibu" in new.nick.lower() or "bu" in new.nick.lower()) and ("cosmos" in new.nick.lower() or "cosmo" in new.nick.lower()):
                 await new.edit(nick="😡 | Gross")
+
 
 def setup(bot):
     bot.add_cog(MyFun(bot))
