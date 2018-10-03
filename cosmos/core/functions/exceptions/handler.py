18b72cb120b47df8ee0568f6784306ded8364e82
deepakrajko14 <deepakrajko14@gmail.com>
2018-10-03 14:03:33 +0000

    An attempt to make sentry functions club with error handler

--- cosmos/core/functions/exceptions/handler.py
+++ cosmos/core/functions/exceptions/handler.py
@@ -1,11 +1,12 @@
 import sentry_sdk as sentry
 
 
-class ExceptionHandler(sentry):
+class ExceptionHandler(object):
 
     def __init__(self, bot):
         self.bot = bot
+        self.sentry = sentry
         try:
-            super().init(self.bot.configs.sentry.dsn)
+            self.sentry.init(self.bot.configs.sentry.dsn)
         except self.utils.BadDsn:
             self.bot.log.error("Invalid sentry DSN provided.")
