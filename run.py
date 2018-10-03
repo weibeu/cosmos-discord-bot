a289202224f394ef0c2b1e3be91085a846c01aed
deepakrajko14 <deepakrajko14@gmail.com>
2018-10-03 14:41:38 +0000

    Initialise sentry integration here to make access to all modules

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from cosmos.core.cosmos import Cosmos
 
 if __name__ == "__main__":
     bot = Cosmos()
+    try:
+        bot.eh.sentry.init(bot.configs.sentry.dsn)  # Initialise sentry integration.
+    except bot.eh.sentry.utils.BadDsn:
+        bot.log.error("Invalid sentry DSN provided.")
     bot.log.info(f"All initial tasks completed. [{bot.time.round_time()} seconds.]")
     bot.run()
