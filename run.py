60199e3e6709bc6d2dd33bc309c38f5c3e23d722
deepakrajko14 <deepakrajko14@gmail.com>
2018-10-06 16:19:04 +0000

    Load sentry with various configurations

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from cosmos.core.cosmos import Cosmos
 if __name__ == "__main__":
     bot = Cosmos()
     try:
-        bot.eh.sentry.init(bot.configs.sentry.dsn)  # Initialise sentry integration.
+        bot.eh.sentry.init(**bot.configs.sentry.raw)  # Initialise sentry integration.
     except bot.eh.sentry.utils.BadDsn:
         bot.log.error("Invalid sentry DSN provided.")
     bot.log.info(f"All initial tasks completed. [{bot.time.round_time()} seconds.]")
