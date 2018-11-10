d7e9c1906821e1a3d768e01861846d7f89685e58
thecosmos <deepakrajko14@gmail.com>
2018-11-10 17:27:22 +0000

    Clean FileHandler base class.

--- cosmos/core/utilities/handlers.py
+++ cosmos/core/utilities/handlers.py
@@ -18,5 +18,8 @@ class FileHandler(object):
                 self.bot.log.info(f"Unsupported file specified. Ignoring {path}.")
             return file_data    # dict
         except FileNotFoundError:
-            print(f"Unable to find specified file '{path}'.")
+            self.bot.log.error(f"Unable to find specified file '{path}'.")
+            return
+        except IsADirectoryError:
+            self.bot.log.error(f"'{path}' must be path to data file not directory.")
             return
