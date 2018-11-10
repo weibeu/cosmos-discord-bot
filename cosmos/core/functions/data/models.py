4321d9004b6f4912e4fa8dca2ca8476cfdb1b600
thecosmos <deepakrajko14@gmail.com>
2018-11-10 17:28:03 +0000

    Clean PluginData base class.

---
+++ cosmos/core/functions/data/models.py
@@ -0,0 +1,19 @@
+import os
+
+from cosmos.core.functions.data.data import Data
+
+
+class PluginData(Data):
+
+    DATA_DIR = "data/"
+
+    def __init__(self, bot, plugin):
+        self.bot = bot
+        self.plugin = plugin
+        self.data_dir = None    # Raw data.
+        self.fetch_plugin_dir()
+        super().__init__(self.bot, self.data_dir)   # Vomit plugin data directory to Data superclass.
+
+
+    def fetch_plugin_dir(self):
+        self.data_dir = os.path.join(self.plugin.dir_path, self.DATA_DIR)
