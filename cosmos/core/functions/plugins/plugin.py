fb37c416cd89cf98a54a37459cf29ce05d07a305
thecosmos <deepakrajko14@gmail.com>
2018-11-10 17:29:08 +0000

    Automatically load plugin data while init. TODO: load, unload methods.

--- cosmos/core/functions/plugins/plugin.py
+++ cosmos/core/functions/plugins/plugin.py
@@ -1,9 +1,23 @@
+from cosmos.core.functions.data.models import PluginData
+
+
 class Plugin(object):
 
-    def __init__(self, bot):
+    def __init__(self, bot, dir_path):
         self.bot = bot
+        self.dir_path = dir_path
         self.name = None
-        self.raw_path = None
+        self.raw_path = None    # path to setup.py file.
         self.python_path = None
         self.category = None
-        # self.data = None
+        self.data = None
+        self.get_data()
+
+    def get_data(self):
+        self.data = PluginData(self.bot, self)
+
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
