4a7a06eb572c01375f8ba0ac8de5bc332c54e5fb
thecosmos <deepakrajko14@gmail.com>
2018-11-11 10:25:59 +0000

    Plugin now looks itself for its own details. load, unload methods.

--- cosmos/core/functions/plugins/plugin.py
+++ cosmos/core/functions/plugins/plugin.py
@@ -1,7 +1,12 @@
+import os
+
+from discord import ClientException
 from cosmos.core.functions.data.models import PluginData
 
 
 class Plugin(object):
+
+    SETUP_FILE = "setup.py"
 
     def __init__(self, bot, dir_path):
         self.bot = bot
@@ -9,15 +14,30 @@ class Plugin(object):
         self.name = None
         self.raw_path = None    # path to setup.py file.
         self.python_path = None
-        self.category = None
+        # self.category = None
         self.data = None
+        self.get_details()
         self.get_data()
+
+    def get_details(self):
+        self.name = os.path.basename(self.dir_path)
+        self.raw_path = os.path.join(self.dir_path, self.SETUP_FILE)
+        self.python_path = f"{self.raw_path.replace('/', '.')}"[:-3]
 
     def get_data(self):
         self.data = PluginData(self.bot, self)
 
     def load(self):
-        pass
+        try:
+            self.bot.load_extension(self.python_path)
+            self.bot.plugins.loaded_plugins.append(self)
+            self.bot.log.info(f"Plugin '{self.name}' loaded.")
+        except ImportError:
+            self.bot.log.info(f"Plugin '{self.name}' failed to load.")
+        except ClientException:
+            self.bot.log.info(f"Can't find setup function in '{self.name}' plugin.")
 
     def unload(self):
-        pass
+        self.bot.unload_extension(self.python_path)
+        self.bot.plugins.loaded_plugins.remove(self)
+        self.bot.log.info(f"Plugin '{self.name}' unloaded.")
