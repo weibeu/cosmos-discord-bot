b2db687127a6fd622b15a4aeb5b2cf9210a8002c
thecosmos <deepakrajko14@gmail.com>
2018-11-15 20:06:51 +0530

    presence data and set presence method

--- cosmos/core/plugins/presence/presence.py
+++ cosmos/core/plugins/presence/presence.py
@@ -6,9 +6,20 @@ class Presence(object):
     def __init__(self, bot):
         self.bot = bot
 
-    async def set_presence(self, activity):
+    async def set_presence(self, activity_type, **kwargs):
+        if isinstance(activity_type, str):
+            try:
+                activity_type = getattr(discord.ActivityType, activity_type)
+            except AttributeError:
+                self.bot.log.error("Unknown discord.ActivityType specified.")
+                self.bot.eh.sentry.capture_exception()
         try:
-            await self.bot.change_presence(activity=activity)
+            if activity_type == discord.ActivityType.playing:
+                await self.bot.change_presence(activity=discord.Game(**kwargs))
+            elif activity_type == discord.ActivityType.streaming:
+                await self.bot.change_presence(activity=discord.Streaming(**kwargs))
+            else:
+                await self.bot.change_presence(activity=discord.Activity(type=activity_type, **kwargs))
         except discord.errors.InvalidArgument:
             self.bot.eh.sentry.capture_exception()
 
