b696aaab7bfa4976c7ec717cd524eb07a3d10e68
thecosmos <deepakrajko14@gmail.com>
2018-11-18 01:07:51 +0530

    Inherit common utility methods

--- cosmos/core/utilities/handler.py
+++ cosmos/core/utilities/handler.py
@@ -1,7 +1,8 @@
+from cosmos.core.utilities.utils import Utils
 from cosmos.core.utilities import handlers
 
 
-class Utility(object):
+class Utility(Utils):
 
     def __init__(self, bot):
         self.bot = bot
