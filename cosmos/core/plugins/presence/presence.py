98078711606aa47063feb2521410d18befbc4468
thecosmos <deepakrajko14@gmail.com>
2018-11-21 16:47:41 +0530

    Final basic presence plugin

--- cosmos/core/plugins/presence/presence.py
+++ cosmos/core/plugins/presence/presence.py
@@ -1,4 +1,7 @@
 import discord
+import random
+
+import asyncio
 
 
 class Presence(object):
@@ -6,8 +9,10 @@ class Presence(object):
     def __init__(self, plugin):
         self.plugin = plugin
         self.bot = self.plugin.bot
+        self.rotate = self.plugin.data.configs.rotate
+        self.bot.loop.create_task(self.rotate_presence())
 
-    async def set_presence(self, activity_type, **kwargs):
+    async def set_presence(self, activity_type, message, **kwargs):
         if isinstance(activity_type, str):
             try:
                 activity_type = getattr(discord.ActivityType, activity_type)
@@ -16,13 +21,34 @@ class Presence(object):
                 self.bot.eh.sentry.capture_exception()
         try:
             if activity_type == discord.ActivityType.playing:
-                await self.bot.change_presence(activity=discord.Game(**kwargs))
+                await self.bot.change_presence(activity=discord.Game(message, **kwargs))
             elif activity_type == discord.ActivityType.streaming:
-                await self.bot.change_presence(activity=discord.Streaming(**kwargs))
+                stream_url = self.plugin.data.configs.stream_url
+                await self.bot.change_presence(activity=discord.Streaming(name=message, url=stream_url, **kwargs))
             else:
-                await self.bot.change_presence(activity=discord.Activity(type=activity_type, **kwargs))
+                await self.bot.change_presence(activity=discord.Activity(type=activity_type, name=message, **kwargs))
         except discord.errors.InvalidArgument:
             self.bot.eh.sentry.capture_exception()
 
-    """async def rotate_presence(self):
-        # rotate presence on certain time interval."""
+    def _get_args(self):
+        activity = self.plugin.data.configs.activity_type
+        if activity.upper() == "RANDOM":
+            activity_type = random.choice(list(self.plugin.data.messages.__dict__))
+            message = random.choice(getattr(self.plugin.data.messages, activity_type))
+            return tuple([activity_type, message])
+        else:
+            try:
+                message = random.choice(getattr(self.plugin.data.messages, activity))
+            except AttributeError:
+                self.bot.log.error(f"Can't find list of messages for '{activity}'.")
+                self.bot.eh.sentry.capture_exception()
+            else:
+                activity_type = activity
+                return tuple([activity_type, message])
+
+    async def rotate_presence(self):
+        await self.bot.wait_until_ready()
+        while self.rotate:
+            args = self._get_args()
+            await self.set_presence(*args)
+            await asyncio.sleep(self.plugin.data.configs.interval)
