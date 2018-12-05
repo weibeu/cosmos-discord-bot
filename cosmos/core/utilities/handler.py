420e5f3fea34638a81e27448a5a099910198f02e
thecosmos <deepakrajko14@gmail.com>
2018-12-05 14:26:41 +0530

    Naisu file handler which doesn't silently ignores errors and doesn't needs bot for no reason

--- cosmos/core/utilities/handler.py
+++ cosmos/core/utilities/handler.py
@@ -1,9 +1,9 @@
-from cosmos.core.utilities.utils import Utils
 from cosmos.core.utilities import handlers
+from cosmos.core.utilities.utils import Utils
 
 
 class Utility(Utils):
 
     def __init__(self, bot):
         self.bot = bot
-        self.file_handler = handlers.FileHandler(self.bot)
+        self.file_handler = handlers.FileHandler()
--- cosmos/core/utilities/handlers.py
+++ cosmos/core/utilities/handlers.py
@@ -1,28 +1,18 @@
 import json
+
 import yaml
 
 
 class FileHandler(object):
 
-    def __init__(self, bot):
-        self.bot = bot
-
-    def get_file_data(self, path):
-        file_data = None
-        try:
-            file = open(path)
-            if path.endswith(".json"):
-                file_data = json.load(file)
-            elif path.endswith(".yaml") or path.endswith(".yml"):
-                file_data = yaml.load(file)
-            else:
-                self.bot.log.info(f"Unsupported file specified. Ignoring {path}.")
-            return file_data    # dict
-        except FileNotFoundError:
-            self.bot.log.error(f"Unable to find specified file '{path}'.")
-            self.bot.eh.sentry.capture_exception()
-            return
-        except IsADirectoryError:
-            self.bot.log.error(f"'{path}' must be path to data file not directory.")
-            self.bot.eh.sentry.capture_exception()
-            return
+    @staticmethod
+    def get_file_data(path):
+        file_data = dict()
+        file = open(path)
+        if path.endswith(".json"):
+            file_data = json.load(file)
+        elif path.endswith(".yaml") or path.endswith(".yml"):
+            file_data = yaml.load(file)
+        else:
+            pass
+        return file_data  # dict
