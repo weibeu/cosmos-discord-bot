63a1477d76d93344752674cd49d24a08b99cc9ab
thecosmos <deepakrajko14@gmail.com>
2018-12-28 10:21:01 +0530

    loggger.error already sends event to sentry so no use of capture_exception

--- cosmos/core/functions/cacher/handler.py
+++ cosmos/core/functions/cacher/handler.py
@@ -24,5 +24,4 @@ class CacheHandler(object):
             self.bot.log.error("Unable to connect to redis server. Check if it's running.")
             self.bot.log.info("Using cachers.AsyncDictCache instead.")
             self.bot.log.warning("Most of the functions of web client will not work.")
-            self.bot.eh.sentry.capture_exception()
             self.redis = cachers.AsyncDictCache()   # Temporarily use normal dictionary if can't reach redis servers.
