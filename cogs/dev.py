cba26369ad877c783620aa944713e56be7c056bd
deepakrajko14 <deepakrajko14@gmail.com>
2019-01-15 23:22:42 +0000

    datetime fixes

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 
 from discord.ext import commands
 
@@ -12,12 +13,16 @@ class DevCommands(object):
     async def ss_msg(self, ctx, message_id, channel: discord.TextChannel = None):
         channel = channel or ctx.channel
         message = await channel.get_message(message_id)
+        if (message.created_at - datetime.datetime.now()).days <= 6:
+            time_stamp = message.created_at.strftime("%A at %I:%M %p")
+        else:
+            time_stamp = message.created_at.strftime("%m/%d/%Y")
         payload = {
             "name": message.author.nick or message.author.name,
             "message_content": message.clean_content,
             "avatar_url": message.author.avatar_url,
             "name_color": list(message.author.color.to_rgb()),
-            "time_stamp": message.created_at.strftime("%A at %I:%M %p")
+            "time_stamp": time_stamp
         }
         async with self.bot.session.post("http://127.0.0.1:5000/discord/ss/message/", json=payload) as response:
             screenshot = await response.read()
