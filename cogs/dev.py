d75613ea5bf587b40188c2800c94bfc2494157b9
deepakrajko14 <deepakrajko14@gmail.com>
2019-01-15 20:23:17 +0000

    Screenshot message command

--- cogs/dev.py
+++ cogs/dev.py
@@ -12,6 +12,18 @@ class DevCommands(object):
     async def ss_msg(self, ctx, message_id, channel: discord.TextChannel = None):
         channel = channel or ctx.channel
         message = await channel.get_message(message_id)
+        payload = {
+            "name": ctx.author.nick,
+            "message_content": message.clean_content,
+            "avatar_url": ctx.author.avatar_url,
+            "name_color": ctx.author.color.to_rgb(),
+            "time_stamp": message.created_at.strftime("%A at %I:%M %p")
+        }
+        async with bot.session.post("http://127.0.0.1:5000/discord/ss/message/", json=payload) as response:
+            screenshot = await response.read()
+        
+        file = discord.File(screenshot, filename="Screenshot.png")
+        await ctx.send(file=file)
         
 
 
