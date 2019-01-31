a36b8f252966c5b3ca1f900f7eea22b45c16f318
thecosmos <deepakrajko14@gmail.com>
2019-01-31 15:25:29 +0530

    Initialise database before initialising caches

--- cosmos/core/cosmos.py
+++ cosmos/core/cosmos.py
@@ -28,8 +28,8 @@ class Cosmos(commands.Bot):
         self.session = aiohttp.ClientSession(loop=self.loop)
         self._init_logger()
         self._init_exception_handler()
-        self._init_caches()
         self._init_database()
+        self._init_caches()
         self._init_emotes()
         self._init_plugins()
         self._init_theme()
@@ -67,15 +67,15 @@ class Cosmos(commands.Bot):
             self.log.error("Invalid sentry DSN provided.")
 
     @Time.calc_time
-    def _init_caches(self):
-        self.log.info("Initialising caches.")
-        self.cache = CacheHandler(self)
-
-    @Time.calc_time
     def _init_database(self):
         self.log.info("Initialising database.")
         self.db_client = DatabaseClient(self)
         self.db = self.db_client.db
+
+    @Time.calc_time
+    def _init_caches(self):
+        self.log.info("Initialising caches.")
+        self.cache = CacheHandler(self)
 
     @Time.calc_time
     def _init_emotes(self):
