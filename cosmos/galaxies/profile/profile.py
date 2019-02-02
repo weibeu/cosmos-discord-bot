f3dddd6caa99344106ec15f11e606bae18b0b0f6
thecosmos <deepakrajko14@gmail.com>
2019-02-02 16:20:34 +0530

    Default profile description

--- cosmos/galaxies/profile/profile.py
+++ cosmos/galaxies/profile/profile.py
@@ -14,23 +14,5 @@ class Profile(Cog):
 
     @commands.command()
     async def profile(self, ctx):
-        profile = await self.cache.get_profile(ctx.author.id)
-        if profile:
-            embed = self.bot.theme.embeds.primary(title="Profile")
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-            embed.add_field(name="Reputation points", value=str(profile.reps))
-            embed.add_field(name="Experience points", value=str(profile.xp))
-            if profile.description:
-                embed.add_field(name="Profile description", value=profile.description)
-            await ctx.send(embed=embed)
-        else:
-            async with ctx.loading():
-                await ctx.send(embed=self.bot.theme.embeds.one_line.primary("Welcome! Creating your Cosmos profile."))
-                profile = await self.cache.create_profile(ctx.author.id)
-                embed = self.bot.theme.embeds.primary(title="Profile")
-                embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-                embed.add_field(name="Reputation points", value=str(profile.reps))
-                embed.add_field(name="Experience points", value=str(profile.xp))
-                if profile.description:
-                    embed.add_field(name="Profile description", value=profile.description)
-                await ctx.send(embed=embed)
+        embed = await self.cache.get_profile_embed(ctx)
+        await ctx.send(embed=embed)
