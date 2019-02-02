d8df16c66bc46e4ca39dfba51d636c936aab39bb
thecosmos <deepakrajko14@gmail.com>
2019-02-02 17:25:17 +0530

    Added icon support in one line embeds

--- cosmos/core/functions/theme/models/embeds/one_line.py
+++ cosmos/core/functions/theme/models/embeds/one_line.py
@@ -3,12 +3,15 @@ from .primary import Primary
 
 class OneLinePrimary(Primary):
 
-    def __init__(self, bot, content: str = None, emote: str = str(), color=None, **kwargs):
+    def __init__(self, bot, content: str, **kwargs):
         self._bot = bot
-        if emote:
-            content = f"{emote}    {content}"
         assert len(content) <= 256, "Content should be less than or equal to 256 in length."
-        super().__init__(title=content, color=color, **kwargs)
+        icon_url = kwargs.get("icon_url")
+        if icon_url:
+            super().__init__(**kwargs)
+            self.set_author(name=content, icon_url=icon_url)
+        else:
+            super().__init__(title=content, **kwargs)
 
     @property
     def bot(self):
@@ -20,9 +23,9 @@ class OneLine(object):
     def __init__(self, bot):
         self.bot = bot
 
-    def primary(self, content: str, emote: str = str(), **kwargs):
-        return OneLinePrimary(self.bot, emote, content, **kwargs)
+    def primary(self, content: str, icon_url=None, **kwargs):
+        return OneLinePrimary(self.bot, content, icon_url=icon_url, **kwargs)
 
-    def discord(self, content: str, emote: str = str(), **kwargs):
+    def discord(self, content: str, icon_url=None, **kwargs):
         discord_color = self.bot.configs.color_scheme.discord
-        return OneLinePrimary(self.bot, emote, content, color=discord_color, **kwargs)
+        return OneLinePrimary(self.bot, content, icon_url=icon_url, color=discord_color, **kwargs)
