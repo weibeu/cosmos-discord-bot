b5ef74a6209bf129971ead63c0de12a4a61b392d
thecosmos <deepakrajko14@gmail.com>
2019-02-04 17:04:02 +0530

    Pre cache static caches

--- cosmos/core/functions/cacher/handler.py
+++ cosmos/core/functions/cacher/handler.py
@@ -7,18 +7,14 @@ class CacheHandler(object):
 
     def __init__(self, bot):
         self.bot = bot
-        self.dict = None
-        self.ttl = None
-        self.lru = None
-        self.lfu = None
         self.redis = None
-        self.bot.loop.create_task(self.__fetch_cachers())
-
-    async def __fetch_cachers(self):
         self.dict = cachers.DictCache
         self.ttl = cachers.TTLCache
         self.lru = cachers.LRUCache
         self.lfu = cachers.LFUCache
+        self.bot.loop.create_task(self.__fetch_redis_client())
+
+    async def __fetch_redis_client(self):
         try:
             conn = await aioredis.connection.create_connection("redis://localhost", loop=self.bot.loop)
             self.redis = cachers.RedisCache(conn)
