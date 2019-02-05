3ab5e3f00c56d0a7d66c167d322f48a4fb63f9fb
thecosmos <deepakrajko14@gmail.com>
2019-02-05 18:03:50 +0530

    Ignore dickheads

--- cosmos/galaxies/profile/profile.py
+++ cosmos/galaxies/profile/profile.py
@@ -13,7 +13,16 @@ class Profile(Cog):
         if self.plugin.data.profile.__dict__.get("cache_all"):
             self.bot.loop.create_task(self.cache.prepare())
 
+    def __is_ignored(self, message):
+        if message.author.id == self.bot.user.id:
+            return True
+        if message.author.bot:
+            return True
+
     async def on_message(self, message):
+        if self.__is_ignored(message):
+            return
+
         await self.cache.give_xp(message)
 
     @commands.command()
