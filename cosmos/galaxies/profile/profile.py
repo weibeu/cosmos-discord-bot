90ab771ae69ae2dcd7c89ff61f647870348145dc
thecosmos <deepakrajko14@gmail.com>
2019-02-07 00:59:22 +0530

    Minor formatting

--- cosmos/galaxies/profile/profile.py
+++ cosmos/galaxies/profile/profile.py
@@ -39,17 +39,17 @@ class Profile(Cog):
     @commands.command(name="rep")
     async def rep_user(self, ctx, user: discord.User):
         if user.bot:
-            embed = self.bot.theme.embeds.one_line.primary("😔 Sorry but I just can't do that.")
+            embed = self.bot.theme.embeds.one_line.primary("😔    Sorry but I just can't do that.")
             return await ctx.send(embed=embed)
         author_profile = await self.cache.get_profile(ctx.author.id)
         if author_profile.can_rep:
             target_profile = await self.cache.get_profile(user.id)
             if not target_profile:
-                res = f"Sorry but, {user.name} hasn't created their Cosmos Profile yet. Can you encourage them to?"
+                res = f"😔    Sorry but, {user.name} hasn't created their Cosmos Profile yet. Can you encourage them to?"
                 embed = self.bot.theme.embeds.one_line.primary(res)
                 await ctx.send(embed=embed)
             await target_profile.rep(author_profile)
-            embed = self.bot.theme.embeds.one_line.primary(f"👌 You added one reputation point to {user.name}.")
+            embed = self.bot.theme.embeds.one_line.primary(f"👌    You added one reputation point to {user.name}.")
             await ctx.send(embed=embed)
         else:
             hrs, mins, secs = author_profile.rep_delta
