f04b3b12607e1c90f23ce77ab0e76b8d2516df85
thecosmos <deepakrajko14@gmail.com>
2019-02-07 18:18:26 +0530

    Bosons

--- cosmos/galaxies/profile/models/currency.py
+++ cosmos/galaxies/profile/models/currency.py
@@ -1,5 +1,7 @@
-from abc import ABC
+import asyncio
+import random
 
+from abc import ABC
 from .base import ProfileModelsBase
 
 
@@ -7,7 +9,19 @@ class Boson(ProfileModelsBase, ABC):
 
     def __init__(self, bosons: int):
         self._bosons = bosons
+        self.in_boson_buffer = False
 
     @property
     def bosons(self):
         return self._bosons
+
+    def give_bosons(self, bosons: int):
+        self._bosons += int(bosons)
+
+    async def give_default_bosons(self):
+        bosons = random.randint(self._plugin.data.boson.default_min, self._plugin.data.boson.default_max)
+        self.give_bosons(bosons)
+
+        self.in_boson_buffer = True
+        await asyncio.sleep(self._plugin.data.boson.buffer_cooldown)
+        self.in_boson_buffer = False
