6c11afd59e9b8e50d2ff23ad0fd23c9493a73037
thecosmos <deepakrajko14@gmail.com>
2019-02-07 20:40:42 +0530

    Response changes

--- cosmos/galaxies/profile/economy.py
+++ cosmos/galaxies/profile/economy.py
@@ -14,10 +14,10 @@ class Economy(Cog):
     @commands.command(name="bosons", aliases=["boson", "$"])
     async def bosons(self, ctx, user: discord.User = None):
         if user:
-            adverb = f"👈    has"
+            adverb = f"{user.name} has"
         else:
-            adverb = "You have"
             user = ctx.author
+            adverb = "{user.name}, you have"
         if user.bot:
             res = f"😙    Poor {user.name} is jobless. Help them to get one."
             return await ctx.send(embed=self.bot.theme.embeds.one_line.primary(res))
@@ -25,5 +25,5 @@ class Economy(Cog):
         if not profile:
             res = self.plugin.data.responses.no_profile.format(user_name=user.name)
             return await ctx.send(embed=self.bot.theme.embeds.one_line.primary(res))
-        res = f"{adverb} {profile.bosons}."
-        await ctx.send(embed=self.bot.theme.embeds.one_line.primary(res, user.avatar_url))
+        res = f"💵    {adverb} {profile.bosons} Bosons."
+        await ctx.send(embed=self.bot.theme.embeds.one_line.primary(res))
