a980999704713a02fbf43deafa7d76a98c8448d2
thecosmos <deepakrajko14@gmail.com>
2019-02-08 23:05:34 +0530

    Marriage properties

--- cosmos/galaxies/profile/models/marriage.py
+++ cosmos/galaxies/profile/models/marriage.py
@@ -5,18 +5,21 @@ from .base import ProfileModelsBase
 class CosmosMarriage(ProfileModelsBase, ABC):
 
     def __init__(self, **kwargs):
-        self.proposed = None
-        self.proposer = None
-        self.spouse = None
-
-    async def __fetch_marriage(self, **kwargs):
         raw_marriage = kwargs.get("marriage", dict())
-        proposed_id = raw_marriage.get("proposed")
-        proposer_id = raw_marriage.get("proposer")
-        spouse_id = raw_marriage.get("spouse")
-        if proposed_id:
-            self.proposed = await self._plugin.profile_cache.get_profile(proposed_id)
-        if proposer_id:
-            self.proposer = await self._plugin.profile_cache.get_profile(proposer_id)
-        if spouse_id:
-            self.spouse = await self._plugin.profile_cache.get_profile(spouse_id)
+        self.proposed_id = raw_marriage.get("proposed")
+        self.proposer_id = raw_marriage.get("proposer")
+        self.spouse_id = raw_marriage.get("spouse")
+
+    # Return discord.User instead of CosmosUserProfile because it would summon three more users to cache.
+
+    @property
+    def proposed(self):
+        return self._plugin.bot.get_user(self.proposed_id)
+
+    @property
+    def proposer(self):
+        return self._plugin.bot.get_user(self.proposer_id)
+
+    @property
+    def spouse(self):
+        return self._plugin.bot.get_user(self.spouse_id)
