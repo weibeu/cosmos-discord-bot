cbfe5a60082b13c9cb7261cd6dea10cdcd3f16a7
thecosmos <deepakrajko14@gmail.com>
2019-02-09 16:36:45 +0530

    Registered marriage cog

--- cosmos/galaxies/profile/__init__.py
+++ cosmos/galaxies/profile/__init__.py
@@ -1,12 +1,15 @@
 from .profile import Profile
 from .economy import Economy
 from .ascension import Levels
+from .marriage import Marriage
+
 from .models import ProfileCache
 
 __all__ = [
     Profile,
     Levels,
-    Economy
+    Economy,
+    Marriage
 ]
 
 
