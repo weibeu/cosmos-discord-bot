9e82e03bd2bccb94d84ca99f5a4a3a8d6e5ffe7c
thecosmos <deepakrajko14@gmail.com>
2019-02-12 16:00:53 +0530

    Initialise fermion class

--- cosmos/galaxies/profile/models/user_profile.py
+++ cosmos/galaxies/profile/models/user_profile.py
@@ -34,6 +34,7 @@ class CosmosUserProfile(UserExperience, Boson, Fermion, CosmosMarriage):
     def __init__(self, plugin, **kwargs):
         UserExperience.__init__(self, **kwargs)
         Boson.__init__(self, **kwargs)
+        Fermion.__init__(**kwargs)
         CosmosMarriage.__init__(self, **kwargs)
         self.__plugin = plugin
         self._id: int = kwargs["user_id"]
