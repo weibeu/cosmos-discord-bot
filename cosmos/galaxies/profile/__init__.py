3f1b6db66cf065dbff931e500daa3b5cd1c56d6a
thecosmos <deepakrajko14@gmail.com>
2019-02-12 22:47:39 +0530

    Don't need to pass bot to batch anymore

--- cosmos/galaxies/profile/__init__.py
+++ cosmos/galaxies/profile/__init__.py
@@ -17,7 +17,7 @@ def setup(bot):
     # plugin = bot.plugins.setup(__file__, cache=ProfileCache)    # setup method automatically passes plugin.
     plugin = bot.plugins.get_from_file(__file__)
     plugin.collection = bot.db[plugin.data.profile.collection_name]
-    plugin.batch = bot.db_client.batch(bot, plugin.collection)
+    plugin.batch = bot.db_client.batch(plugin.collection)
     plugin.cache = ProfileCache(plugin)
 
     plugin.load_cogs(__all__)
