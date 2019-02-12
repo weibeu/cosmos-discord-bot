ea9d9b5d2938d2ae3097755187a2874f71cb18f9
thecosmos <deepakrajko14@gmail.com>
2019-02-12 22:46:14 +0530

    Pass bot in core itself

--- cosmos/core/functions/database/database.py
+++ cosmos/core/functions/database/database.py
@@ -10,9 +10,11 @@ class DatabaseClient(AsyncIOMotorClient):
         self.database_name = self.bot.configs.db.database
         super().__init__(self.uri)
         self.db = self.get_database()
-        self.batch = DatabaseBatch
 
     def get_database(self, database_name: str = None):
         name = self.database_name or database_name
         database = self[name]
         return database
+
+    def batch(self, collection):
+        return DatabaseBatch(self.bot, collection)
