d6e85c072b0c61cdad79e5cfda39a7809d9eb70b
thecosmos <deepakrajko14@gmail.com>
2019-02-12 23:09:23 +0530

    Public fetch_emotes

--- cosmos/core/functions/emotes/emotes.py
+++ cosmos/core/functions/emotes/emotes.py
@@ -5,9 +5,9 @@ class CosmosEmotes(object):
 
     def __init__(self, bot):
         self.bot = bot
-        self.bot.loop.create_task(self.__fetch_emotes())
+        self.bot.loop.create_task(self.fetch_emotes())
 
-    async def __fetch_emotes(self):
+    async def fetch_emotes(self):
         await self.bot.wait_until_ready()
         for guild_name, guild_id in self.bot.configs.emotes.raw.items():
             guild = self.bot.get_guild(guild_id)
