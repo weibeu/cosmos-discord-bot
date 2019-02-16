659ab611e6d2a3c9e453bd2989ab83978afd5c61
thecosmos <deepakrajko14@gmail.com>
2019-02-16 17:34:40 +0530

    from arrow

--- cosmos/galaxies/profile/models/family.py
+++ cosmos/galaxies/profile/models/family.py
@@ -12,7 +12,7 @@ class CosmosMarriage(ProfileModelsBase, ABC):
         self.proposed_id = raw_marriage.get("proposed")
         self.proposer_id = raw_marriage.get("proposer")
         self.spouse_id = raw_marriage.get("spouse")
-        self.marriage_timestamp = raw_marriage.get("timestamp")
+        self.marriage_timestamp = self.get_arrow(raw_marriage.get("timestamp"))
 
     # Return discord.User instead of CosmosUserProfile because it would summon three more users to cache.
 
