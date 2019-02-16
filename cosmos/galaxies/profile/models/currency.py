d9141b37bb0f9e850ba2a717994d5118cf7228b5
thecosmos <deepakrajko14@gmail.com>
2019-02-16 10:55:28 +0530

    Attribute name change

--- cosmos/galaxies/profile/models/currency.py
+++ cosmos/galaxies/profile/models/currency.py
@@ -11,7 +11,7 @@ class Boson(ProfileModelsBase, ABC):
     def __init__(self, **kwargs):
         raw_currency = kwargs.get("currency", dict())
         self._bosons = raw_currency.get("bosons", 0)
-        self.boson_daily_datetime = raw_currency.get("daily_datetime")
+        self.boson_daily_timestamp = raw_currency.get("boson_daily_timestamp")
 
         self.in_boson_buffer = False
 
@@ -32,21 +32,21 @@ class Boson(ProfileModelsBase, ABC):
 
     @property
     def can_take_daily_bosons(self):
-        if not self.boson_daily_datetime:
+        if not self.boson_daily_timestamp:
             return True
-        delta = arrow.utcnow() - self.boson_daily_datetime
+        delta = arrow.utcnow() - self.boson_daily_timestamp
         return delta.seconds >= self._plugin.data.boson.daily_cooldown*60*60
 
     @property
     def daily_bosons_delta(self) -> tuple:
-        return self.time_delta(self.boson_daily_datetime, self._plugin.data.boson.daily_cooldown)
+        return self.time_delta(self.boson_daily_timestamp, self._plugin.data.boson.daily_cooldown)
 
     async def take_daily_bosons(self, target_profile=None):
         profile = target_profile or self
         profile._bosons += self._plugin.data.boson.default_daily
-        self.boson_daily_datetime = arrow.utcnow()
+        self.boson_daily_timestamp = arrow.utcnow()
         await self._collection.update_one(
-            {"user_id": self.id}, {"$set": {"currency.daily_datetime": self.boson_daily_datetime.datetime}}
+            {"user_id": self.id}, {"$set": {"currency.boson_daily_timestamp": self.boson_daily_timestamp.datetime}}
         )
 
 
