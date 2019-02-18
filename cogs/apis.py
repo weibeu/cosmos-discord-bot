00247b417dd051d2d95f6c5a9b2ca3204d70d4e6
thecosmos <deepakrajko14@gmail.com>
2019-02-18 12:02:41 +0530

    Fux FUx

--- cogs/apis.py
+++ cogs/apis.py
@@ -25,12 +25,15 @@ class API:
         except asyncurban.WordNotFoundError:
             return await ctx.send(f"Unable to find `{term}` from Urban Dictionary.")
         async def show_help(p):
-            p.embed.description = ""
-            p.embed.add_field(name="Examples", value=word.example)
+            if not p.example:
+                p.embed.add_field(name="Examples", value=word.example)
+                p.example = True
+            else:
+                p.embed.clear_fields()
+                p.example = False
             await p.message.edit(embed=p.embed)
-            await asyncio.sleep(60)
-            await p.show_current_page()
         Pages.show_help = show_help
+        Pages.example = False
         paginator = Pages(ctx, entries=word.definition.splitlines(), per_page=8, show_entry_count=False, show_author=False)
         paginator.embed.set_author(name=f"{word.word} - Urban Dictionary", url=word.permalink, icon_url=ctx.author.avatar_url)
 
