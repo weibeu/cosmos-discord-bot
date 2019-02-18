5b64b2fb679d23f51e2b8a010d2ce911e4293d71
thecosmos <deepakrajko14@gmail.com>
2019-02-18 11:54:34 +0530

    Fix

--- cogs/apis.py
+++ cogs/apis.py
@@ -24,14 +24,16 @@ class API:
             word = await self.ud.get_word(term)
         except asyncurban.WordNotFoundError:
             return await ctx.send(f"Unable to find `{term}` from Urban Dictionary.")
-        paginator = Pages(ctx, entries=word.definition.splitlines(), per_page=8, show_entry_count=False, show_author=False)
-        paginator.embed.set_author(name=f"{word.word} - Urban Dictionary", url=word.permalink, icon_url=ctx.author.avatar_url)
         async def show_help(p):
             p.embed.description = p.Empty
             p.embed.add_field(name="Examples", value=word.example)
             await p.message.edit(embed=p.embed)
             await asyncio.sleep(60)
             await p.show_current_page()
+        Pages.show_help = show_help
+        paginator = Pages(ctx, entries=word.definition.splitlines(), per_page=8, show_entry_count=False, show_author=False)
+        paginator.embed.set_author(name=f"{word.word} - Urban Dictionary", url=word.permalink, icon_url=ctx.author.avatar_url)
+
         paginator.show_help = show_help
         await paginator.paginate()
 
