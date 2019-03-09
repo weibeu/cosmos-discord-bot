1ef11ea65e9eb986ae2ed715202da8c90b239a8e
thecosmos <deepakrajko14@gmail.com>
2019-03-09 20:40:09 +0530

    Incomplete BaseMenu

--- cosmos/core/functions/context/functions/menus.py
+++ cosmos/core/functions/context/functions/menus.py
@@ -1,7 +1,35 @@
 from .paginators import BasePaginator
 
 
+class MenuEntry(object):
+
+    async def __default_parser(self):
+        return str(self.raw)
+
+    def __init__(self, ctx, raw, entry_parser=None):
+        self.raw = raw
+        self.string_parser = entry_parser or self.__default_parser
+        self.string = str()
+        ctx.bot.loop.create_task(self.__fetch_string())
+
+    def __str__(self):
+        return self.string
+
+    async def __fetch_string(self):
+        self.string = await self.string_parser(self.raw)
+
+
 class BaseMenu(BasePaginator):
 
-    def __init__(self, ctx):
-        super().__init__(ctx)
+    def __init__(self, ctx, entries, entry_parser=None, *args, **kwargs):
+        self.ctx = ctx
+        self.raw_entries = entries
+        self.entry_parser = entry_parser
+        self.entries = []
+        self.fetch_entries()
+        super().__init__(self.ctx, self.entries, *args, **kwargs)
+
+    def fetch_entries(self):
+        for raw_entry in self.raw_entries:
+            entry = MenuEntry(self.ctx, raw_entry, self.entry_parser)
+            self.entries.append(entry)
