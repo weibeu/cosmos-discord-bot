18a1cf88f5c022fb624b57bc8f8147371e8c0151
thecosmos <deepakrajko14@gmail.com>
2019-03-15 13:53:27 +0530

    IncompleTe base menu

--- cosmos/core/functions/context/functions/menus.py
+++ cosmos/core/functions/context/functions/menus.py
@@ -6,30 +6,33 @@ class MenuEntry(object):
     async def __default_parser(self, *_, **__):
         return str(self.raw)
 
-    def __init__(self, ctx, raw, entry_parser=None):
+    def __init__(self, raw, emote, entry_parser=None):
         self.raw = raw
+        self.emote = emote
         self.string_parser = entry_parser or self.__default_parser
         self.string = str()
-        ctx.bot.loop.create_task(self.__fetch_string())
 
-    def __str__(self):
-        return self.string
-
-    async def __fetch_string(self):
-        self.string = await self.string_parser(self.raw)
+    async def get_string(self):
+        return await self.string_parser(self.raw)
 
 
 class BaseMenu(BasePaginator):
 
-    def __init__(self, ctx, entries, entry_parser=None, *args, **kwargs):
+    def __init__(self, ctx, entries, entry_parser=None, per_page=12, *args, **kwargs):
         self.ctx = ctx
         self.raw_entries = entries
         self.entry_parser = entry_parser
+        self.per_page = per_page
         self.entries = []
+        self.bullets = self.ctx.bot.emotes.foods.emotes
         self.fetch_entries()
-        super().__init__(self.ctx, self.entries, show_bullets=True, *args, **kwargs)
+        super().__init__(self.ctx, self.entries, self.per_page, is_menu=True, *args, **kwargs)
 
     def fetch_entries(self):
+        counter = 0
         for raw_entry in self.raw_entries:
-            entry = MenuEntry(self.ctx, raw_entry, self.entry_parser)
+            entry = MenuEntry(raw_entry, self.bullets[counter], self.entry_parser)
             self.entries.append(entry)
+            counter += 1
+            if counter == self.per_page:
+                counter = 0
