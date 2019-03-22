c7dbd9a4b2662d2a4bd06e9e3900471e3b84b76f
thecosmos <deepakrajko14@gmail.com>
2019-03-22 11:44:54 +0530

    Guild Profile Base

---
+++ cosmos/galaxies/profile/models/profiles/guild/base.py
@@ -0,0 +1,36 @@
+from abc import abstractmethod
+
+from ..base import ProfileModelsBase
+
+
+class GuildProfileModelsBase(ProfileModelsBase):
+
+    @property
+    @abstractmethod
+    def profile(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def guild_id(self):
+        raise NotImplementedError
+
+    @property
+    def plugin(self):
+        return self.profile.plugin
+
+    @property
+    def collection(self):
+        return self.profile.collection
+
+    @property
+    def id(self):
+        return self.profile.id
+
+    @property
+    def is_prime(self):
+        return self.profile.is_prime
+
+    @property
+    def guild_filter(self):
+        return f"guilds.{self.guild_id}"
