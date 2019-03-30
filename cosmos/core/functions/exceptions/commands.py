5e5c9898d832f7db43576a734ad4f4c3782352c1
thecosmos <deepakrajko14@gmail.com>
2019-03-30 09:15:31 +0530

    NotPrime exceptions

--- cosmos/core/functions/exceptions/commands.py
+++ cosmos/core/functions/exceptions/commands.py
@@ -1,6 +1,16 @@
 from discord.ext import commands
 
 
+class GuildNotPrime(commands.CheckFailure):
+
+    pass
+
+
+class MemberNotPrime(commands.CheckFailure):
+
+    pass
+
+
 class MemberMissingPermissions(commands.MissingPermissions):
 
     pass
