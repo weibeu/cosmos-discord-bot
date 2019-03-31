3cc2a1cd015426c5a36df6a6ef3518de482fafb1
thecosmos <deepakrajko14@gmail.com>
2019-03-31 14:21:36 +0530

    Fixes

--- cosmos/galaxies/guild/settings/settings.py
+++ cosmos/galaxies/guild/settings/settings.py
@@ -29,8 +29,8 @@ class GuildSettings(Cog):
         res = f"{ctx.guild.name} custom prefixes: {prefixes}."
         await ctx.send_line(res, ctx.guild.icon_url)
 
-    @prefix.command(name="set", ignore_extra=False)
-    async def set_prefix(self, ctx, *, prefix):
+    @prefix.command(name="set")
+    async def set_prefix(self, ctx, prefix):
         if not await self.__check_prefix(ctx, prefix):
             return
         self.cache.prefixes.set(ctx.guild.id, [prefix])
@@ -40,8 +40,8 @@ class GuildSettings(Cog):
         await ctx.send_line(f"{prefix} has been set as custom prefix of {ctx.guild.name}.", ctx.guild.icon_url)
 
     @Cog.checks.prime_guild()
-    @prefix.command(name="add", ignore_extra=False)
-    async def add_prefix(self, ctx, *, prefix):
+    @prefix.command(name="add")
+    async def add_prefix(self, ctx, prefix):
         if not await self.__check_prefix(ctx, prefix):
             return
         prefixes = self.cache.prefixes.get(ctx.guild.id, list())
@@ -57,7 +57,7 @@ class GuildSettings(Cog):
             f"{prefix} has been added to list of custom prefixes of {ctx.guild.name}.", ctx.guild.icon_url)
 
     @prefix.command(name="remove", aliases=["delete"])
-    async def remove_prefix(self, ctx, *, prefix, ignore_extra=False):
+    async def remove_prefix(self, ctx, prefix):
         prefixes = self.cache.prefixes.get(ctx.guild.id, list())
         try:
             prefixes.remove(prefix)
