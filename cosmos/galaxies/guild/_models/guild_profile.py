6e30fd719393960b7ea42128ff0a09180cdad00f
thecosmos <deepakrajko14@gmail.com>
2019-04-09 22:23:08 +0530

    Added channel option

--- cosmos/galaxies/guild/_models/guild_profile.py
+++ cosmos/galaxies/guild/_models/guild_profile.py
@@ -25,10 +25,11 @@ class CosmosGuild(GuildSettings):
         self.is_prime = kwargs.get("is_prime", False)
         GuildSettings.__init__(self, **kwargs)
 
-    async def send_welcome_banner(self, member: discord.Member):
+    async def send_welcome_banner(self, member: discord.Member, channel: discord.TextChannel = None):
+        channel = channel or self.welcome_banner_channel
         banner_bytes = await self.plugin.bot.image_processor.get_welcome_banner(
             self.welcome_banner_url, member.avatar_url, member.name, self.welcome_banner_text
         )
         banner_format = self.welcome_banner_url.split(".")[-1]
         file = discord.File(BytesIO(banner_bytes), filename=f"{self.plugin.data.settings.banner_name}.{banner_format}")
-        await self.welcome_banner_channel.send(file=file)
+        await channel.send(file=file)
