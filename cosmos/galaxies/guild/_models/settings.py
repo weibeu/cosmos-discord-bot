b02c3d38da0342db6476f9c3d71deba6f44bc323
thecosmos <deepakrajko14@gmail.com>
2019-04-09 22:00:11 +0530

    Added channel support for welcome banners

--- cosmos/galaxies/guild/_models/settings.py
+++ cosmos/galaxies/guild/_models/settings.py
@@ -10,17 +10,24 @@ class WelcomeBannerSettings(CosmosGuildBase, ABC):
         raw_banner_settings = raw_welcome_settings.get("banner", dict())
         self.welcome_banner_url = raw_banner_settings.get("url", str())
         self.welcome_banner_text = raw_banner_settings.get("text", str())
+        self.__welcome_banner_channel_id = raw_banner_settings.get("channel", int())
         self.welcome_banner_enabled = raw_banner_settings.get("enabled", False)
 
-    async def set_welcome_banner(self, banner_url, text):
+    @property
+    def welcome_banner_channel(self):
+        return self.guild.get_channel(self.__welcome_banner_channel_id)
+
+    async def set_welcome_banner(self, banner_url, text, channel_id):
         self.welcome_banner_url = banner_url
         self.welcome_banner_text = text
+        self.__welcome_banner_channel_id = channel_id
         self.welcome_banner_enabled = False
 
         await self.collection.update_one(
             self.document_filter, {"$set": {
                 "settings.welcome.banner.url": self.welcome_banner_url,
                 "settings.welcome.banner.text": self.welcome_banner_text,
+                "settings.welcome.banner.channel": self.__welcome_banner_channel_id,
                 "settings.welcome.banner.enabled": self.welcome_banner_enabled
             }}
         )
