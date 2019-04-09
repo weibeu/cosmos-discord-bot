188ae73dfc67cf08b935c19751f731b7019e3275
thecosmos <deepakrajko14@gmail.com>
2019-04-09 19:53:19 +0530

    We can pass plugin object since using dict as cache

--- cosmos/galaxies/guild/_models/base.py
+++ cosmos/galaxies/guild/_models/base.py
@@ -3,27 +3,26 @@ from abc import ABC, abstractmethod
 
 class CosmosGuildBase(ABC):
 
-    # @property
-    # @abstractmethod
-    # def plugin(self):
-    #     raise NotImplementedError
-    #
-    # @property
-    # @abstractmethod
-    # def collection(self):
-    #     raise NotImplementedError
+    @property
+    @abstractmethod
+    def plugin(self):
+        raise NotImplementedError
+
+    @property
+    def collection(self):
+        return self.plugin.collection
 
     @property
     @abstractmethod
     def id(self):
         raise NotImplementedError
 
-    # @property
-    # def guild(self):
-    #     return self.plugin.bot.get_guild(self.id)
-    #
-    # @property
-    # def document_filter(self):
-    #     return {
-    #         "guild_id": self.id
-    #     }
+    @property
+    def guild(self):
+        return self.plugin.bot.get_guild(self.id)
+
+    @property
+    def document_filter(self):
+        return {
+            "guild_id": self.id
+        }
--- cosmos/galaxies/guild/_models/guild_profile.py
+++ cosmos/galaxies/guild/_models/guild_profile.py
@@ -3,23 +3,20 @@ from .settings import GuildSettings
 
 class CosmosGuild(GuildSettings):
 
-    # @property
-    # def plugin(self):
-    #     return self.__plugin
-    #
-    # @property
-    # def collection(self):
-    #     pass
+    @property
+    def plugin(self):
+        return self.__plugin
 
     @property
     def id(self):
         return self.__id
 
     @classmethod
-    def from_document(cls, document: dict):
-        return cls(**document)
+    def from_document(cls, plugin, document: dict):
+        return cls(plugin, **document)
 
-    def __init__(self, **kwargs):
+    def __init__(self, plugin, **kwargs):
+        self.__plugin = plugin
         self.__id = kwargs["guild_id"]
         self.is_prime = kwargs.get("is_prime", False)
         GuildSettings.__init__(self, **kwargs)
