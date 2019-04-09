35c028f27a0a2f38bff9853004c02db750752203
thecosmos <deepakrajko14@gmail.com>
2019-04-09 23:46:11 +0530

    Accept name, avatar url not member object

--- cosmos/galaxies/guild/_models/guild_profile.py
+++ cosmos/galaxies/guild/_models/guild_profile.py
@@ -25,10 +25,10 @@ class CosmosGuild(GuildSettings):
         self.is_prime = kwargs.get("is_prime", False)
         GuildSettings.__init__(self, **kwargs)
 
-    async def send_welcome_banner(self, member: discord.Member, channel: discord.TextChannel = None):
+    async def send_welcome_banner(self, name, avatar_url, channel: discord.TextChannel = None):
         channel = channel or self.welcome_banner_channel
         banner_bytes = await self.plugin.bot.image_processor.get_welcome_banner(
-            self.welcome_banner_url, member.avatar_url, member.name, self.welcome_banner_text
+            self.welcome_banner_url, avatar_url, name, self.welcome_banner_text
         )
         banner_format = self.welcome_banner_url.split(".")[-1]
         file = discord.File(BytesIO(banner_bytes), filename=f"{self.plugin.data.settings.banner_name}.{banner_format}")
