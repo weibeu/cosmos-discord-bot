bcfb57ae28ba66dd4a014b6bbe497eef5b9f6da7
thecosmos <deepakrajko14@gmail.com>
2019-04-09 20:21:52 +0530

    Set welcome banner method

--- cosmos/galaxies/guild/_models/settings.py
+++ cosmos/galaxies/guild/_models/settings.py
@@ -3,7 +3,28 @@ from abc import ABC
 from .base import CosmosGuildBase
 
 
-class GuildSettings(CosmosGuildBase, ABC):
+class WelcomeBannerSettings(CosmosGuildBase, ABC):
+
+    def __init__(self, **kwargs):
+        raw_welcome_settings = kwargs.get("welcome", dict())
+        raw_banner_settings = raw_welcome_settings.get("banner", dict())
+        self.welcome_banner_url = raw_banner_settings.get("url", str())
+        self.welcome_banner_text = raw_banner_settings.get("text", str())
+
+    async def set_welcome_banner(self, banner_url, text):
+        self.welcome_banner_url = banner_url
+        self.welcome_banner_text = text
+
+        await self.collection.update_one(
+            self.document_filter, {"$set": {
+                "settings.welcome.banner.url": self.welcome_banner_url,
+                "settings.welcome.banner.text": self.welcome_banner_text
+            }}
+        )
+
+
+class GuildSettings(WelcomeBannerSettings, ABC):
 
     def __init__(self, **kwargs):
         raw_settings = kwargs.get("settings", dict())
+        super().__init__(raw_settings)
