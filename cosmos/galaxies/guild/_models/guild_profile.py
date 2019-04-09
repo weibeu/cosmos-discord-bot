a051219dcee6b4c8024eae485dbb45572efcc4af
thecosmos <deepakrajko14@gmail.com>
2019-04-09 22:18:57 +0530

    send_banner method

--- cosmos/galaxies/guild/_models/guild_profile.py
+++ cosmos/galaxies/guild/_models/guild_profile.py
@@ -1,3 +1,7 @@
+from io import BytesIO
+
+import discord
+
 from .settings import GuildSettings
 
 
@@ -20,3 +24,11 @@ class CosmosGuild(GuildSettings):
         self.__id = kwargs["guild_id"]
         self.is_prime = kwargs.get("is_prime", False)
         GuildSettings.__init__(self, **kwargs)
+
+    async def send_welcome_banner(self, member: discord.Member) -> discord.File:
+        banner_bytes = await self.plugin.bot.image_processor.get_welcome_banner(
+            self.welcome_banner_url, member.avatar_url, member.name, self.welcome_banner_text
+        )
+        banner_format = self.welcome_banner_url.split(".")[-1]
+        file = discord.File(BytesIO(banner_bytes), filename=f"{self.plugin.data.settings.banner_name}.{banner_format}")
+        await self.welcome_banner_channel.send(file=file)
