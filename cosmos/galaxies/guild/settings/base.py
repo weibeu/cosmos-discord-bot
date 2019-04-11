c23f0a58368e342c19d4e500927e4862e600e4e2
thecosmos <deepakrajko14@gmail.com>
2019-04-11 21:52:42 +0530

    Use command local checks for in accordance with other commands also

--- cosmos/galaxies/guild/settings/base.py
+++ cosmos/galaxies/guild/settings/base.py
@@ -1,6 +1,6 @@
 from ... import Cog
 
-from discord.ext.commands import MissingPermissions
+from discord.ext.commands import MissingPermissions, has_permissions
 
 
 class Settings(Cog):
@@ -10,15 +10,16 @@ class Settings(Cog):
         self.plugin = plugin
         self.cache = self.plugin.cache
 
-    def cog_check(self, ctx):
-        if not ctx.author.guild_permissions.manage_guild:
-            raise MissingPermissions(["manage_guild"])
-        return True
+    # def cog_check(self, ctx):
+    #     if not ctx.author.guild_permissions.manage_guild:
+    #         raise MissingPermissions(["manage_guild"])
+    #     return True
 
     async def cog_before_invoke(self, ctx):
         ctx.guild_profile = await self.cache.get_profile(ctx.guild.id)
         # Ensure CosmosGuild is in cache before changing its settings and dynamically pass it to ctx.guild_profile.
 
+    @has_permissions(manage_guild=True)
     @Cog.group(name="welcome", aliases=["join"])
     async def welcome(self, ctx):
         pass
