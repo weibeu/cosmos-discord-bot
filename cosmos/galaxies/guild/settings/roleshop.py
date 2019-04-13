56037bab9025808b643971803b4d21921cff632b
thecosmos <deepakrajko14@gmail.com>
2019-04-14 04:24:59 +0530

    roleshop settings cog

---
+++ cosmos/galaxies/guild/settings/roleshop.py
@@ -0,0 +1,23 @@
+import typing
+
+import discord
+
+from .base import Settings
+
+
+class RoleShopSettings(Settings):
+
+    @Settings.group(name="roleshop", aliases=["shop", "roleshop"])
+    async def roleshop(self, ctx):
+        pass
+
+    @roleshop.command(name="create")
+    async def create_role(self, ctx, points: int, *, role: typing.Union[discord.Role, str]):
+        # if len(ctx.guild_profile.roleshop) >= self.plugin.data.roleshop.max_roles:
+        #     res = f"❌    Sorry but role shop can't have more than {self.plugin.data.roleshop.max_roles} roles."
+        #     return await ctx.send_line(res)
+
+        if isinstance(role, str):
+            role = await ctx.guild.create_role(name=role, reason=f"Role created for role shop. [{ctx.author}]")
+        await ctx.guild_profile.roleshop.create_role(role.id, points)
+        await ctx.send_line(f"✅    Added {role.name} to role shop for {points} points.")
--- cosmos/galaxies/guild/settings/settings.py
+++ cosmos/galaxies/guild/settings/settings.py
@@ -1,7 +1,8 @@
 from .prefix import PrefixSettings
 from .banner import BannerSettings
+from .roleshop import RoleShopSettings
 
 
-class GuildSettings(PrefixSettings, BannerSettings):
+class GuildSettings(PrefixSettings, BannerSettings, RoleShopSettings):
 
     pass
