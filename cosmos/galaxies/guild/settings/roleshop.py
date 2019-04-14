12bc5da126ca0af4361efd0c9ed84989b8f82ded
thecosmos <deepakrajko14@gmail.com>
2019-04-14 22:12:29 +0530

    We can now implement max allowed roles

--- cosmos/galaxies/guild/settings/roleshop.py
+++ cosmos/galaxies/guild/settings/roleshop.py
@@ -13,9 +13,9 @@ class RoleShopSettings(Settings):
 
     @role_shop.command(name="create")
     async def create_role(self, ctx, points: int, *, role: typing.Union[discord.Role, str]):
-        # if len(ctx.guild_profile.roleshop) >= self.plugin.data.roleshop.max_roles:
-        #     res = f"❌    Sorry but role shop can't have more than {self.plugin.data.roleshop.max_roles} roles."
-        #     return await ctx.send_line(res)
+        if len(ctx.guild_profile.roleshop.roles) >= self.plugin.data.roleshop.max_roles:
+            res = f"❌    Sorry but role shop can't have more than {self.plugin.data.roleshop.max_roles} roles."
+            return await ctx.send_line(res)
 
         if isinstance(role, str):
             role = await ctx.guild.create_role(name=role, reason=f"Role created for role shop. [{ctx.author}]")
