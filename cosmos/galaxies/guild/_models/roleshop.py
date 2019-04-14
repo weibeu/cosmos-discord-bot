39d2f296567866eb489af483fecf558229f22186
thecosmos <deepakrajko14@gmail.com>
2019-04-14 22:09:16 +0530

    Clean base model

--- cosmos/galaxies/guild/_models/roleshop.py
+++ cosmos/galaxies/guild/_models/roleshop.py
@@ -5,8 +5,20 @@ from .base import CosmosGuildBase
 
 class RoleShopRole(object):
 
-    def __init__(self, **kwargs):
+    def __init__(self, role_id, **kwargs):
+        self.id = role_id
         self.points = kwargs["points"]
+
+
+class Roles(dict):
+
+    def get(self, role_id):
+        return RoleShopRole(role_id, **super().get(str(role_id)))
+
+    def set(self, role_id, **kwargs):
+        raw = super().get(str(role_id), dict())
+        raw.update(kwargs)
+        self.update({str(role_id): raw})
 
 
 class RoleShop(object):
@@ -14,14 +26,10 @@ class RoleShop(object):
     def __init__(self, guild_profile, **kwargs):
         self.__profile = guild_profile
         raw_roleshop = kwargs.get("roleshop", dict())
-        self.__raw_roles = raw_roleshop.get("roles", dict())
+        self.roles = Roles(raw_roleshop.get("roles", dict()))
 
     async def create_role(self, role_id, points,):
-        raw_role = {
-            "points": points,
-            # "prime": True,
-        }
-        self.__raw_roles.update({role_id: raw_role})
+        self.roles.set(role_id, points=points)
 
         self.__profile.collection.update_one(
             self.__profile.document_filter, {"$set": {
