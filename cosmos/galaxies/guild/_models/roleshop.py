04d6343cd7a73e7fb073ffb9aaaf22c8377b8d45
thecosmos <deepakrajko14@gmail.com>
2019-04-15 22:38:01 +0530

    RoleShopRoles - model changed to list. remove_role - new method

--- cosmos/galaxies/guild/_models/roleshop.py
+++ cosmos/galaxies/guild/_models/roleshop.py
@@ -5,24 +5,27 @@ from .base import CosmosGuildBase
 
 class RoleShopRole(object):
 
-    def __init__(self, role_id, **kwargs):
-        self.id = role_id
+    def __init__(self, **kwargs):
+        self.id = kwargs["role_id"]
         self.points = kwargs["points"]
 
+    @property
+    def document(self):
+        return {
+            "role_id": self.id,
+            "points": self.points,
+        }
 
-class Roles(dict):
+
+class Roles(list):
 
     def get(self, role_id):
-        return RoleShopRole(role_id, **super().get(str(role_id)))
+        for role_document in self:
+            if role_document["role_id"] == role_id:
+                return RoleShopRole(**role_document)
 
-    def set(self, role_id, **kwargs):
-        raw = super().get(str(role_id), dict())
-        raw.update(kwargs)
-        self.update({str(role_id): raw})
-
-    def __iter__(self):
-        for key in super().__iter__():
-            yield int(key)
+    def remove(self, role_id):
+        super().remove(self.get(role_id))
 
 
 class RoleShop(object):
@@ -30,15 +33,27 @@ class RoleShop(object):
     def __init__(self, guild_profile, **kwargs):
         self.__profile = guild_profile
         raw_roleshop = kwargs.get("roleshop", dict())
-        self.roles = Roles(raw_roleshop.get("roles", dict()))
+        self.roles = Roles(raw_roleshop.get("roles", list()))
 
-    async def create_role(self, role_id, points,):
-        self.roles.set(role_id, points=points)
+    async def create_role(self, role_id, points):
+        role_document = {
+            "role_id": role_id,
+            "points": points,
+        }
+        self.roles.append(role_document)
 
         self.__profile.collection.update_one(
-            self.__profile.document_filter, {"$set": {
-                f"roleshop.roles.{role_id}.points": points,
-                # f"roleshop.roles.{role_id}.prime": True,
+            self.__profile.document_filter, {"$addToSet": {
+                "roleshop.roles": role_document
+            }}
+        )
+
+    async def remove_role(self, role_id):
+        self.roles.remove(role_id)
+
+        self.__profile.collection.update_one(
+            self.__profile.document_filter, {"$pull": {
+                "roleshop.roles": {"role_id": role_id}
             }}
         )
 
