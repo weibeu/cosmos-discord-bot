b3557defada1ace39ffb8eadfdb40cd79a0d3b50
thecosmos <deepakrajko14@gmail.com>
2019-04-16 16:54:34 +0530

    Roleshop admin commands

--- cosmos/galaxies/guild/settings/roleshop.py
+++ cosmos/galaxies/guild/settings/roleshop.py
@@ -1,6 +1,5 @@
-import typing
-
 import discord
+import typing
 
 from .base import Settings
 
@@ -10,6 +9,22 @@ class RoleShopSettings(Settings):
     @Settings.roles.group(name="roleshop", aliases=["shop"])
     async def role_shop(self, ctx):
         pass
+
+    async def __get_role(self, ctx, role=None) -> discord.Role:
+        if role:
+            if not ctx.guild_profile.roleshop.has_role(role.id):
+                await ctx.send_line(f"❌    {role.name} is not a role shop role.")
+                raise LookupError
+            return role
+        menu = ctx.get_field_menu(ctx.guild_profile.roleshop.roles, self.__paginator_parser)
+        response = await menu.wait_for_response()
+        return ctx.guild.get_role(response.raw_key)
+
+    @staticmethod
+    async def __paginator_parser(ctx, role_id, role_document):
+        role = ctx.guild.get_role(role_id)
+        points = role_document.get("points")
+        return role.name, f"{ctx.bot.emotes.misc.coins} {points}"
 
     @role_shop.command(name="create")
     async def create_role(self, ctx, points: int, *, role: typing.Union[discord.Role, str]):
@@ -21,3 +36,26 @@ class RoleShopSettings(Settings):
             role = await ctx.guild.create_role(name=role, reason=f"Role created for role shop. [{ctx.author}]")
         await ctx.guild_profile.roleshop.create_role(role.id, points)
         await ctx.send_line(f"✅    Added role {role.name} to role shop with {points} points.")
+
+    @role_shop.command(name="remove", aliases=["delete"])
+    async def delete_role(self, ctx, *, role: discord.Role = None):
+        role = await self.__get_role(ctx, role)
+
+        if await ctx.confirm(f"⚠    Are you sure to delete role {role.name} from role shop?"):
+            await role.delete(reason=f"Role deleted from role shop. [{ctx.author}]")
+            await ctx.guild_profile.roleshop.remove_role(role.id)
+
+            await ctx.send_line(f"✅    Role {role.name} has been removed from role shop.")
+
+    @role_shop.group(name="modify", aliases=["edit"])
+    async def modify_role(self, ctx):
+        pass
+
+    @modify_role.command(name="points", aliases=["point"])
+    async def modify_points(self, ctx, new_points: int, *, role: discord.Role = None):
+        role = await self.__get_role(ctx, role)
+
+        if await ctx.confirm(f"⚠    Are you sure to change points of role {role.name} to {new_points}?"):
+            await ctx.guild_profile.roleshop.set_points(role.id, new_points)
+
+            await ctx.send(f"✅    {role.name} points has been changed to {new_points}.")
