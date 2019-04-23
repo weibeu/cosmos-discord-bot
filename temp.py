8c8e4eaf98caba9e6c19561ece52ddd945ccf411
deepakrajko14 <deepakrajko14@gmail.com>
2019-04-23 04:15:26 +0000

    Added file temp.py

---
+++ temp.py
@@ -0,0 +1,80 @@
+rules = {'color': 15277667,
+ 'fields': [{'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** Discord tos apply**',
+             'value': 'ANY violation will result in an immediate ban and report to Discord: [Discord '
+                      'Tos](https://discordapp.com/tos)'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** Be respectful of others**',
+             'value': 'Bullying, cussing, harassment, racism, sexism, or any sort of discrimination will not be '
+                      'tolerated.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** Use common sense**',
+             'value': 'If someone is uncomfortable with something, stop. Also refrain from discussing real-life '
+                      'Politics, religion, killing or suicide.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** Do not spam**',
+             'value': 'Do not send random or irrelevant messages. This includes text, copypastas, Emojis, images, '
+                      'links, literally anything. This applies to VC too, Ear-raping or being obnoxious in VC will '
+                      'result in punishment.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** DRAMA FREE ZONE**',
+             'value': 'Do not create server drama or take part in it. If you have any problem with the server, talk to '
+                      'the staff via DMs.__**No roleplay**__'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** No NSFW **',
+             'value': 'Keep any and all 18+ flirting, overly sexual discussion, recommendations, images, links, etc. '
+                      'in DM’s only.  NSFW content is not allowed on The Anime Discord and will result in an immediate ban.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** No Mini Modding **',
+             'value': 'Leave all the moderation to Staff. If there is someone causing any trouble DM/Ping any of the '
+                      'Staff Members preferably the online ones.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** Do not advertise or self-promote. **',
+             'value': 'This is not a place to advertise your server, YouTube, Twitch etc. Do not solicit other users '
+                      'for donations or money. You will receive a permanent ban if we determine you are using the '
+                      'server for those purposes.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** No inappropriate profiles **',
+             'value': 'Usernames or avatars considered to be racist, discriminatory, overly lewd or intended to mimic '
+                      'another user are not tolerated and will result in an immediate ban.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>            ** Speak English **',      
+             'value': 'This is an English speaking server.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** Respect channel topics and rules. **',
+             'value': 'Every channel on this server has its own specific guidelines and rules. They can be found in '
+                      'the channel description.'}],
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>            ** No Unbanning **',
+             'value': 'Previously banned users can no longer appeal for an unban.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>      ** Nickname Policy **',
+             'value': 'We follow a nickname policy. Meaning if your nickname is NSFW we can and will change your nickname to something else.'},
+            {'inline': False,
+             'name': '<a:pointer:440925289317531668>    ** Dont organise raids. **',
+             'value': 'Members who are reported/seen organising raids and actually participate in them will be banned.'},
+'footer': {'icon_url': 'https://cdn.discordapp.com/icons/244998983112458240/d2702db072fdba5fd7be9123db9a668d.webp?size=1024',
+            'proxy_icon_url': 'https://images-ext-1.discordapp.net/external/dCF2D_-HwWo85rIAC7x6hdiKSEfwn19Q-EwTBMd3Ri8/%3Fsize%3D1024/https/cdn.discordapp.com/icons/244998983112458240/d2702db072fdba5fd7be9123db9a668d.webp',
+            'text': 'The Anime Discord'},
+ 'title': 'RULES',
+ 'type': 'rich'}
+
+ doggo = {'color': 15277667,
+ 'description': 'If one or more of the rules are broken by a member, the staff will be obligated to follow the bad '
+                'doggo protocol:\n'
+                '\n'
+                '\t<a:arrow:437323076330586113> 😟 = First warning, lasts for 10 days. `+6 Hours doggo`\n'
+                '\t<a:arrow:437323076330586113> 😠 = Second warning, lasts for 20 days. `+24 Hours doggo`\n'
+                '\t<a:arrow:437323076330586113> 😡 = Third warning, lasts for 40 days. `+48 Hours doggo`\n'
+                '\n'
+                '\n'
+                '__ If a member breaks the rules after being given 3rd warning, they will receive a **permanent '
+                'ban**.__\n'
+                '\n'
+                '**Note:**\tA member may get banned despite not having above three warnings if their stay here causes '
+                'a severe harm to the server or members in any possible manner.',
+ 'footer': {'icon_url': 'https://cdn.discordapp.com/icons/244998983112458240/d2702db072fdba5fd7be9123db9a668d.webp?size=1024',
+            'proxy_icon_url': 'https://images-ext-1.discordapp.net/external/dCF2D_-HwWo85rIAC7x6hdiKSEfwn19Q-EwTBMd3Ri8/%3Fsize%3D1024/https/cdn.discordapp.com/icons/244998983112458240/d2702db072fdba5fd7be9123db9a668d.webp',
+            'text': 'The Anime Discord'},
+ 'title': 'PUNISHMENTS',
+ 'type': 'rich'}
