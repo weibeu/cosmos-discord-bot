e6add8954871a4e8b23509dae708b9aa108644f1
thecosmos <deepakrajko14@gmail.com>
2019-04-25 21:25:07 +0530

    transfer points command

--- cosmos/galaxies/guild/roleshop/points.py
+++ cosmos/galaxies/guild/roleshop/points.py
@@ -43,3 +43,17 @@ class RoleShopPoints(RoleShopBase):
         daily_points = await author_profile.take_daily_points(target_profile)
         res = f"🗓    {daily_points} daily points were given to {target_name}."
         await ctx.send_line(res)
+
+    @points.command(name="credit", aliases=["transfer", "give"])
+    async def transfer_points(self, ctx, member: discord.Member, points: int):
+        if member.bot:
+            return await ctx.send_line("❌    You can't transfer points to robos.")
+        if points < 0:
+            return await ctx.send_line("❌    Sorry but I suck at calculations involving negative numbers.")
+        author_profile = await self.bot.profile_cache.get_guild_profile(ctx.author.id, ctx.guild.id)
+        target_profile = await self.bot.profile_cache.get_guild_profile(member.id, ctx.guild.id)
+        if author_profile.points < points:
+            return await ctx.send_line("❌    Sorry but you don't have enough points to complete this transaction.")
+        author_profile.give_points(-points)
+        target_profile.give_points(points)
+        await ctx.send_line(f"📤    {ctx.author.name}, you gave {points} points to {member.display_name}.")
