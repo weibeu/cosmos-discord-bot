41d6a9b82db7c0f0cd53aa9f434d7dce72938b83
thecosmos <deepakrajko14@gmail.com>
2019-04-29 15:09:58 +0530

    Minor fix

--- cosmos/galaxies/profile/models/profiles/guild/roleshop.py
+++ cosmos/galaxies/profile/models/profiles/guild/roleshop.py
@@ -11,5 +11,5 @@ class MemberRoleShop(object):
             self.__fetch_roles(raw_roleshop.get("roles", list())))
 
     async def __fetch_roles(self, raw_roles):
-        roles = (await self.profile.fetch_guild_profile()).roles
+        roles = (await self.profile.fetch_guild_profile()).roleshop.roles
         self.roles = [role for role in roles if role.id in raw_roles]
