461ee7cc43da2b5f2b2f18bd1ac26eaddda99ee9
thecosmos <deepakrajko14@gmail.com>
2019-04-29 14:58:21 +0530

    Buy command - roleshop

--- cosmos/galaxies/guild/roleshop/roleshop.py
+++ cosmos/galaxies/guild/roleshop/roleshop.py
@@ -8,8 +8,16 @@ class RoleShop(RoleShopPoints, RoleShopSettings):
 
     @RoleShopSettings.role_shop.command(name="buy", aliases=["purchase"])
     async def buy_role(self, ctx, role: discord.Role = None):
-        roleshop = ctx.guild_profile.roleshop
-
+        profile = await self.bot.profile_cache.get_guild_profile(ctx.author.id, ctx.guild.id)
+        roles = [role for role in ctx.guild_profile.roleshop.roles if role not in profile.roleshop.roles]
+        role = await self._get_role(ctx, role, roles)
+        if role.id in profile.roleshop.roles:
+            return await ctx.send_line(f"❌    You have already purchased {role.name}.")
+        if await ctx.confirm(f"⚠    Are you sure to purchase {role.name}?"):
+            await ctx.guild_profile.roleshop.buy_role(profile, role.id)
+            await ctx.send_line(f"✅    {role.name} purchased. Now you're left with {profile.points} guild points.")
+            if await ctx.confirm(f"❓    Equip {role.name} right now?"):
+                await ctx.author.add_roles(role, reason="Role purchased from role shop.")
 
     @buy_role.error
     async def buy_error(self, ctx, error):
