f68a9e2d07eec24bcce4f5b55e0e65040ec27049
thecosmos <deepakrajko14@gmail.com>
2019-05-16 00:24:52 +0530

    Basic Level cog

--- cosmos/galaxies/guild/levels/levels.py
+++ cosmos/galaxies/guild/levels/levels.py
@@ -1,8 +1,16 @@
-import typing
 import discord
 
 from .._models.base import GuildBaseCog
-from discord.ext.commands import has_permissions, Greedy
+from discord.ext.commands import has_permissions, Converter, RoleConverter, BadArgument
+
+
+class _RoleConvertor(Converter):
+
+    async def convert(self, ctx, argument):
+        try:
+            return [await RoleConverter().convert(ctx, argument)]
+        except BadArgument:
+            return [await RoleConverter().convert(ctx, raw_role) for raw_role in argument.split()]
 
 
 class Levels(GuildBaseCog):
@@ -22,19 +30,19 @@ class Levels(GuildBaseCog):
         _reward = ctx.guild_profile.levels.rewards.get(level)
         if not _reward:
             return await ctx.send_line(f"❌    There are no rewards assigned for level {level}.")
-        embed = self.bot.theme.embeds.primary(title=f"Rewards - Level {level}")
+        embed = self.bot.theme.embeds.primary()
+        embed.set_author(name=f"Rewards for Level {level}", icon_url=ctx.guild.icon_url)
         embed.add_field(name="Roles", value=" ".join([f"<@&{role_id}>" for role_id in _reward.roles]))
-        if _reward.points:
-            embed.add_field(name="Points", value=_reward.points)
+        embed.add_field(name="Points", value=_reward.points)
         await ctx.send(embed=embed)
 
     @rewards.command(name="set")
     @has_permissions(administrator=True)
-    async def set_rewards(self, ctx, level: int, points: typing.Optional[int] = 0, *, roles: Greedy[discord.Role]):
-        embed = self.bot.theme.embeds.primary(title=f"Are you sure to set following rewards for Level {level}?")
+    async def set_rewards(self, ctx, level: int, points: int, *, roles: _RoleConvertor()):
+        embed = self.bot.theme.embeds.primary()
+        embed.set_author(name=f"Are you sure to set following rewards for Level {level}?", icon_url=ctx.guild.icon_url)
         embed.add_field(name="Roles", value=" ".join([role.mention for role in roles]))
-        if points:
-            embed.add_field(name="Points", value=points)
+        embed.add_field(name="Points", value=points)
         if await ctx.confirm(await ctx.send(embed=embed)):
             await ctx.guild_profile.levels.set_rewards(level, [role.id for role in roles], points)
             await ctx.send_line(f"✅    Rewards for Level {level} has been set.")
