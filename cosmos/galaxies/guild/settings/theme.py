1d6ee30b7f1add0af81bc1c9772d3d1981c866ab
thecosmos <deepakrajko14@gmail.com>
2019-05-30 17:26:19 +0530

    Add separate set command

--- cosmos/galaxies/guild/settings/theme.py
+++ cosmos/galaxies/guild/settings/theme.py
@@ -8,19 +8,20 @@ from .base import Settings
 class ThemeSettings(Settings):
 
     @Settings.group(name="theme", aliases=["themes"])
-    @has_permissions(manage_guild=True)
     async def theme_setting(self, ctx):
         pass
 
-    @theme_setting.command(name="color", aliases=["colour"])
-    async def theme_color(self, ctx, color: discord.Color = None):
-        if not color:
-            if ctx.guild_profile.theme.color:
-                res = f"{ctx.guild.name} theme color is set to {ctx.guild_profile.theme.color}."
-            else:
-                res = f"{ctx.guild.name} theme color isn't set yet."
-            return await ctx.send_line(res, ctx.guild.icon_url)
+    @theme_setting.group(name="color", aliases=["colour"], invoke_without_command=True)
+    async def theme_color(self, ctx):
+        if ctx.guild_profile.theme.color:
+            res = f"{ctx.guild.name} theme color is set to {ctx.guild_profile.theme.color}."
+        else:
+            res = f"{ctx.guild.name} theme color isn't set yet."
+        return await ctx.send_line(res, ctx.guild.icon_url)
 
+    @theme_color.command(name="set")
+    @has_permissions(manage_guild=True)
+    async def set_theme_color(self, ctx, color: discord.Color):
         await ctx.guild_profile.theme.set_color(color)
         await ctx.send_line(
             f"{ctx.guild.name} theme color has been set to {ctx.guild_profile.theme.color}.", ctx.guild.icon_url
