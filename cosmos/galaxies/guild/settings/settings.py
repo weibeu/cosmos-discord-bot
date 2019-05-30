248898c4007cec78ffea8f7af82c9346e28f464f
thecosmos <deepakrajko14@gmail.com>
2019-05-30 16:43:56 +0530

    Add theme settings to cog

--- cosmos/galaxies/guild/settings/settings.py
+++ cosmos/galaxies/guild/settings/settings.py
@@ -1,7 +1,8 @@
+from .theme import ThemeSettings
 from .prefix import PrefixSettings
 from .banner import BannerSettings
 
 
-class GuildSettings(PrefixSettings, BannerSettings):
+class GuildSettings(ThemeSettings, PrefixSettings, BannerSettings):
 
     pass
---
+++ cosmos/galaxies/guild/settings/theme.py
@@ -0,0 +1,22 @@
+import discord
+
+from .base import Settings
+
+
+class ThemeSettings(Settings):
+
+    @Settings.guild.group(name="theme", aliases=["themes"])
+    async def theme_setting(self, ctx):
+        pass
+
+    @theme_setting.command(name="color", aliases=["colour"])
+    async def theme_color(self, ctx, color: discord.Color = None):
+        if not color:
+            if ctx.guild_profile.theme.color:
+                res = f"{ctx.guild.name} theme color is set to {ctx.guild_profile.theme.color}."
+            else:
+                res = f"{ctx.guild.name} theme color isn't set yet."
+            return await ctx.send_line(res, ctx.guild.icon_url)
+
+        await ctx.guild_profile.theme.set_color(color.value)
+        await ctx.send_line(f"{ctx.guild.name} theme color has been set to {ctx.guild_profile.theme.color}.")
