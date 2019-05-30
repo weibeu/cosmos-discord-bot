bf561e6df4893d4b8b3373edaa33201b5f9c2544
thecosmos <deepakrajko14@gmail.com>
2019-05-30 17:58:32 +0530

    Use ctx shorthand

--- cosmos/galaxies/guild/_models/base.py
+++ cosmos/galaxies/guild/_models/base.py
@@ -35,8 +35,7 @@ class GuildBaseCog(Cog):
     def __init__(self, plugin):
         super().__init__()
         self.plugin = plugin
-        self.cache = self.plugin.cache
 
     async def cog_before_invoke(self, ctx):
-        ctx.guild_profile = await self.cache.get_profile(ctx.guild.id)
+        ctx.guild_profile = await ctx.fetch_guild_profile()
         # Ensure CosmosGuild is in cache before changing its settings and dynamically pass it to ctx.guild_profile.
