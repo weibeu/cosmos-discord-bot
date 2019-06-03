247d50d1c34d125bfefced39b83b046eb4977165
thecosmos <deepakrajko14@gmail.com>
2019-06-03 17:23:58 +0530

    Add send embed methods to events

--- cosmos/galaxies/moderation/logger/events.py
+++ cosmos/galaxies/moderation/logger/events.py
@@ -1,45 +1,99 @@
+import functools
+from datetime import datetime
+
 from ... import Cog
 
 
+def logger_event(*args, **kwargs):
+
+    def decorator(function):
+        @Cog.listener(*args, **kwargs)
+        @functools.wraps(function)
+        async def wrapper(cog, *_args):
+            guild_profile = await cog.bot.guild_cache.get_profile(_args[0].guild.id)
+            logger = guild_profile.get_logger(function.__name__)
+            if not logger:
+                return
+            embed = cog.embed(title=function.__name__.lstrip("on_").replace("_", " ").title())
+            embed.timestamp = datetime.now()
+            embed = function(embed, cog, *_args)
+            await logger.channel.send(embed=embed)
+        return wrapper
+
+    return decorator
+
+
 class LoggerEvents(Cog):
+
+    # TODO: Add reactions based actions.
 
     def __init__(self, plugin):
         super().__init__()
         self.plugin = plugin
         self.loggers = [name for name, _ in self.get_listeners()]
 
-    @Cog.listener()
-    async def on_message_delete(self, message):
-        pass
+    @property
+    def embed(self):
+        return self.bot.theme.embeds.primary()
 
-    @Cog.listener()
-    async def on_bulk_message_delete(self, messages):
-        pass
+    @logger_event()
+    async def on_message_delete(self, embed, message):
+        embed.add_field(name="Deleted Message", value=message.clean_content, inline=False)
+        embed.add_field(name="Author", value=message.author.mention)
+        embed.add_field(name="From Channel", value=message.channel.mention)
+        return embed
 
-    @Cog.listener()
-    async def on_message_edit(self, before, after):
-        pass
+    @logger_event()
+    async def on_bulk_message_delete(self, embed, messages):
+        embed.add_field(name="Messages Deleted", value=f"{len(messages)} messages.")
+        if messages:
+            embed.add_field(name="From Channel", value=messages[0].channel.mention)
+        return embed
 
-    @Cog.listener()
-    async def on_guild_channel_pins_update(self, channel, last_pin):
-        pass
+    @logger_event()
+    async def on_message_edit(self, embed, before, after):
+        embed.add_field(name="Before Edit", value=before.clean_content, inline=False)
+        embed.add_field(name="After", value=before.clean_content, inline=False)
+        embed.add_field(name="Author", value=after.author.mention)
+        embed.add_field(name="In Channel", value=after.channel.mention)
+        return embed
 
-    @Cog.listener()
-    async def on_member_join(self, member):
-        pass
+    @logger_event()
+    async def on_guild_channel_pins_update(self, embed, channel, _):
+        embed.title = "Message Pinned"
+        message = (await channel.pins())[-1]
+        embed.add_field(name="Message Content", value=message.clean_content, inline=False)
+        embed.add_field(name="Author", value=message.author.mention)
+        embed.add_field(name="In Channel", value=channel.mention)
+        embed.add_field(name="Actual Message", value=f"[Jump to the message]({message.jump_url})")
+        return embed
 
-    @Cog.listener()
-    async def on_member_remove(self, member):
-        pass
+    @logger_event()
+    async def on_member_join(self, embed, member):
+        embed.add_field(name="Member", value=member.mention)
+        embed.add_field(name="Member Name", value=member)
+        embed.add_field(name="Member ID", value=f"`{member.id}`")
+        embed.add_field(name="Account Created", value=f"on {member.created_at.strftime('%d %B %Y')}")
+        embed.set_thumbnail(url=member.avatar_url)
+        return embed
 
-    @Cog.listener()
-    async def on_member_ban(self, guild, user):
-        pass
+    @logger_event()
+    async def on_member_remove(self, embed, member):
+        embed.title = "Member Left"
+        embed.color = int("0xF44336", 16)
+        embed.add_field(name="Member Name", value=member)
+        embed.add_field(name="Member ID", value=f"`{member.id}`")
+        embed.set_thumbnail(url=member.avatar_url)
+        return embed
 
-    @Cog.listener()
-    async def on_member_unban(self, guild, user):
-        pass
+    # @logger_event()
+    # async def on_member_ban(self, embed, guild, user):
+    #     pass
+    #
+    # @logger_event()
+    # async def on_member_unban(self, embed, guild, user):
+    #     pass
 
-    @Cog.listener()
-    async def on_moderation(self, member, action):
+    @logger_event()
+    async def on_moderation(self, embed, member, action):
         pass
