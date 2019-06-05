eaf37dc08532982b9a17612e71d8d34e279021fc
thecosmos <deepakrajko14@gmail.com>
2019-06-05 10:40:13 +0530

    Temporarily log command errors to sentry for dev

--- cosmos/core/plugins/error_handler/command.py
+++ cosmos/core/plugins/error_handler/command.py
@@ -1,3 +1,6 @@
+import sys
+import traceback
+
 from ...functions import Cog
 
 
@@ -6,3 +9,9 @@ class CommandErrorHandler(Cog):
     def __init__(self, plugin):
         super().__init__()
         self.plugin = plugin
+
+    @Cog.listener()
+    async def on_command_error(self, ctx, error):
+        self.bot.eh.sentry.capture_exception(error)
+        self.bot.log.error(f"Ignoring exception in command {ctx.command}")
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
