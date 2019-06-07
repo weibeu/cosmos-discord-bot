0bb7e310d2f588e6af95ce8f5d55bcf257f6320b
thecosmos <deepakrajko14@gmail.com>
2019-06-07 22:42:36 +0530

    Add required utility methods

--- cosmos/core/utilities/utils.py
+++ cosmos/core/utilities/utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from .api.hastebin import HasteBin
 
@@ -12,3 +13,23 @@ class Utils(HasteBin):
     @staticmethod
     def get_file_directory(path):
         return os.path.basename(os.path.dirname(path))
+
+    @staticmethod
+    def find_urls(string):
+        return re.findall(r"(http|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", string)
+
+    @staticmethod
+    def find_image_urls(string):
+        return re.findall(r"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)", string)
+
+    def find_urls_and_strip(self, string):
+        urls = self.find_urls(string)
+        for url in urls:
+            string.replace(url, str())
+        return string, urls
+
+    def find_image_urls_and_strip(self, string):
+        urls = self.find_image_urls(string)
+        for url in urls:
+            string.replace(url, str())
+        return string, urls
