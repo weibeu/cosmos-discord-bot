0332d601958877810e84af8874d4eb3a8fd26996
thecosmos <deepakrajko14@gmail.com>
2019-06-10 16:26:54 +0530

    Use instance name

--- cosmos/galaxies/moderation/_models/moderation/moderation.py
+++ cosmos/galaxies/moderation/_models/moderation/moderation.py
@@ -14,7 +14,7 @@ class ModerationAction(object):
     @property
     def document(self):
         _ = {
-            "action": self.action_type.__class__.__name__,
+            "action": self.action_type.__name__,
             "target_id": self.target.id,
             "moderator_id": self.moderator.id,
         }
