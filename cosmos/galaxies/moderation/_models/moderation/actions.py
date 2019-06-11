484486f580637901b5f3d70528eca409fd66b1d2
thecosmos <deepakrajko14@gmail.com>
2019-06-11 11:02:42 +0530

    Add few common actions and __str__ method

--- cosmos/galaxies/moderation/_models/moderation/actions.py
+++ cosmos/galaxies/moderation/_models/moderation/actions.py
@@ -1,3 +1,21 @@
-class Warned(object):
+class ActionsBase(object):
+
+    TITLE = str()
+
+    def __str__(self):
+        return self.TITLE
+
+
+class Warned(ActionsBase):
 
     TITLE = "Member Warned"
+
+
+class Kicked(ActionsBase):
+
+    TITLE = "Member Kicked"
+
+
+class Banned(ActionsBase):
+
+    TITLE = "User Banned"
