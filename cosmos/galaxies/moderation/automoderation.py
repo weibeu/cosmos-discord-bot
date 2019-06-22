826c30a9e6f9955df208a81f57ac0a475d262ba7
thecosmos <deepakrajko14@gmail.com>
2019-06-22 11:53:09 +0530

    Check if certain trigger is set

--- cosmos/galaxies/moderation/automoderation.py
+++ cosmos/galaxies/moderation/automoderation.py
@@ -19,7 +19,7 @@ class AutoModeration(Cog):
         guild_profile = await self.bot.guild_cache.get_profile(message.guild.id)
 
         trigger = guild_profile.auto_moderation.triggers.get("banned_words")
-        if set(message.content.lower().split()) & trigger.banned_words:
+        if trigger and set(message.content.lower().split()) & trigger.banned_words:
             await trigger.dispatch(message.author)
 
     # @Cog.group(name="banword", aliases=["bannedwords", "banwords"], invoke_without_command=True)
