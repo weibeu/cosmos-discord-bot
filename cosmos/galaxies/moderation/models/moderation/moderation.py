bd3315fe506f15ce16a275848cbdbfff253b0811
thecosmos <deepakrajko14@gmail.com>
2019-08-07 21:21:34 +0530

    NVM

--- cosmos/galaxies/moderation/models/moderation/moderation.py
+++ cosmos/galaxies/moderation/models/moderation/moderation.py
@@ -4,8 +4,9 @@ import datetime
 
 class ModerationAction(object):
 
-    def __init__(self, bot, target, moderator, action_type, reason=None):
-        self.bot = bot
+    def __init__(self, guild_profile, target, moderator, action_type, reason=None):
+        self.guild_profile = guild_profile
+        self.bot = self.guild_profile.plugin.bot
         self.target = target
         self.moderator = moderator
         self.action_type = action_type
@@ -29,8 +30,12 @@ class ModerationAction(object):
         _ = self.bot.theme.embeds.one_line.primary(title)
         _.description = f"**Reason:** {self.reason}"
         _.timestamp = datetime.datetime.now()
+        try:
+            profile = await self.guild_profile.fetch_member_profile(self.target.id)
+        except AttributeError:
+            profile = await self.guild_profile.fetch_member_profile(self.target)
         _id = await self.bot.discordDB.set(self.document)
-        await self.target.log_moderation(_id)
+        await profile.log_moderation(_id)
         try:
             return await self.target.send(embed=_)
         except (discord.Forbidden, AttributeError):
