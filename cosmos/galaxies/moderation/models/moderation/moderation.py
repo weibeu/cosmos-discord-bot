1079c85be0a8754489cb2b742de46ded3ef4fcc3
thecosmos <deepakrajko14@gmail.com>
2019-08-07 21:04:13 +0530

    Remove guild_profile parameter

--- cosmos/galaxies/moderation/models/moderation/moderation.py
+++ cosmos/galaxies/moderation/models/moderation/moderation.py
@@ -4,13 +4,12 @@ import datetime
 
 class ModerationAction(object):
 
-    def __init__(self, guild_profile, target, moderator, action_type, reason=None):
-        self.guild_profile = guild_profile
-        self.bot = self.guild_profile.plugin.bot
+    def __init__(self, target, moderator, action_type, reason=None):
         self.target = target
         self.moderator = moderator
         self.action_type = action_type
         self.reason = reason or "Reason not specified."
+        self.bot = self.target.plugin.bot
 
     @property
     def document(self):
@@ -30,12 +29,8 @@ class ModerationAction(object):
         _ = self.bot.theme.embeds.one_line.primary(title)
         _.description = f"**Reason:** {self.reason}"
         _.timestamp = datetime.datetime.now()
-        try:
-            profile = await self.guild_profile.fetch_member_profile(self.target.id)
-        except AttributeError:
-            profile = await self.guild_profile.fetch_member_profile(self.target)
         _id = await self.bot.discordDB.set(self.document)
-        await profile.log_moderation(_id)
+        await self.target.log_moderation(_id)
         try:
             return await self.target.send(embed=_)
         except (discord.Forbidden, AttributeError):
