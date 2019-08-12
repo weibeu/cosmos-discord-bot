b0d12913408d0365b7dab9014ee23c8bb225d936
thecosmos <deepakrajko14@gmail.com>
2019-08-12 22:02:01 +0530

    New message format

--- cosmos/galaxies/moderation/models/automoderation/actions.py
+++ cosmos/galaxies/moderation/models/automoderation/actions.py
@@ -11,11 +11,9 @@ class AutoModerationActions(object):
     @property
     def warning(self):
         try:
-            return "⚠    " + getattr(
-                self._trigger.profile.plugin.data.triggers_warning, self._trigger.name
-            )
+            return getattr(self._trigger.profile.plugin.data.triggers_warning, self._trigger.name)
         except AttributeError:
-            return f"⚠    You are being warned for violating {self._trigger.name}."
+            return f"You are being warned for violating {self._trigger.name}."
 
     @property
     def embed(self):
@@ -31,8 +29,7 @@ class AutoModerationActions(object):
                 message.author.mention, embed=self.embed(self.warning), delete_after=4)
             await ModerationAction(
                 self._trigger.profile, message.author, message.guild.me, actions.Warned(True), self.warning
-            ).dispatch(
-                f"⚠    You were warned in {self._trigger.profile.guild.name} for violating {self._trigger.name}.")
+            ).dispatch(f"⚠    You were warned in {self._trigger.profile.guild.name}.")
 
     async def mute(self, *, member, **kwargs):
         pass
