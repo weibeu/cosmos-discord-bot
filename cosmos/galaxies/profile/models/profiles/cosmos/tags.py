b3cba4430e1ed228616950e8ceb27e216ee1858a
thecosmos <deepakrajko14@gmail.com>
2019-10-20 15:10:30 +0530

    User tags low level implementation

---
+++ cosmos/galaxies/profile/models/profiles/cosmos/tags.py
@@ -0,0 +1,53 @@
+from abc import ABC
+
+from ..base import ProfileModelsBase
+
+
+class Tag(object):
+
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content
+
+    @property
+    def document(self):
+        return {
+            "name": self.name,
+            "content": self.content
+        }
+
+
+class UserTags(ProfileModelsBase, ABC):
+
+    def __init__(self, documents):
+        self.tags = [Tag(_["name"], _["content"]) for _ in documents]
+
+    def get_tag(self, name):
+        try:
+            return [tag for tag in self.tags if tag.name.lower() == name][0]
+        except IndexError:
+            pass
+
+    def __remove_tag(self, name):
+        tag = self.get_tag(name)
+        self.tags.remove(tag)
+
+    async def create_tag(self, name, content):
+        if self.get_tag(name):
+            self.__remove_tag(name)    # Replace tag with new content.
+        tag = Tag(name, content)
+        self.tags.append(tag)
+
+        await self.collection.update_one(
+            self.document_filter, {"$pull": {"tags": {"name": tag.name}}}
+        )    # Pull if already exists.
+        await self.collection.update_one(
+            self.document_filter, {"$addToSet": {"tags": tag.document}}
+        )
+
+    async def remove_tag(self, name):
+        self.__remove_tag(name)
+
+        await self.collection.update_one(
+            self.document_filter, {"$pull": {"tags": {"name": name}}}
+        )
