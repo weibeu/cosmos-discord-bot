ae37d71bd271038a031355f4db4436e88ef6b35e
thecosmos <deepakrajko14@gmail.com>
2019-10-25 01:09:38 +0530

    Motherfucking fix - potato eyes

--- cosmos/core/functions/plugins/models/commands.py
+++ cosmos/core/functions/plugins/models/commands.py
@@ -3,32 +3,32 @@ from discord.ext.commands import Command, Group
 
 class CosmosCommand(Command):
 
-    def __init__(self, inescapable, *args, **kwargs):
-        if not inescapable:
+    def __init__(self, function, inescapable=True, **kwargs):
+        if inescapable:
             self.disabled_channels = []
         self.inescapable = inescapable
-        super().__init__(*args, **kwargs)
+        super().__init__(function, **kwargs)
 
 
 class CosmosGroupCommand(Group):
 
-    def __init__(self, *args, **kwargs):
-        self.disabled_channels = []
+    def __init__(self, inescapable=True, *args, **kwargs):
+        self.inescapable = inescapable
         super().__init__(*args, **kwargs)
 
 
-def cosmos_command(name: str = None, inescapable=True, cls=None, **attrs):
+def cosmos_command(name: str = None, cls=None, **attrs):
 
     cls = cls or CosmosCommand
 
     def decorator(function):
         if isinstance(function, CosmosCommand):
             raise TypeError('Callback is already a command.')
-        return cls(function, name=name, inescapable=inescapable, **attrs)
+        return cls(function, name=name, **attrs)
 
     return decorator
 
 
-def cosmos_group_command(name: str = None, inescapable=True, **attrs):
+def cosmos_group_command(name: str = None, **attrs):
     attrs.setdefault('cls', CosmosGroupCommand)
-    return cosmos_command(name=name, inescapable=inescapable, **attrs)
+    return cosmos_command(name=name, **attrs)
