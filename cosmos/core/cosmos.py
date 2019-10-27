fc7a2a90099bf651f0c6bccb005a06f1d462c871
thecosmos <deepakrajko14@gmail.com>
2019-10-28 00:49:47 +0530

    Add get_galaxy method to fetch that plugin

--- cosmos/core/cosmos.py
+++ cosmos/core/cosmos.py
@@ -16,6 +16,9 @@ class Cosmos(InitialTasks):
             self.log.error("Invalid token provided.")
             raise LoginFailure
 
+    def get_galaxy(self, name):
+        return self.plugins.get(name)
+
     async def on_ready(self):
         self.log.info(f"{self.user.name}#{self.user.discriminator} Ready! [{self.time.round_time()} seconds.]")
         self.log.info(f"User Id: {self.user.id}")
