d62638bc9f91bb12852fafd8aca3c64b5bf7ad79
thecosmos <deepakrajko14@gmail.com>
2019-10-28 00:53:43 +0530

    Generic low level

--- cosmos/galaxies/guild/_models/permissions.py
+++ cosmos/galaxies/guild/_models/permissions.py
@@ -9,13 +9,14 @@ class DisabledFunctions(object):
         self.__bot = bot
         self.__fetch_commands(document.get("commands", dict()))    # {command_name: [channel_ids], }
         self.__fetch_plugins(document.get("plugins", dict()))
+        self.__fetch_galaxies(document.get("galaxies", dict()))
 
     def __get_channels(self, channel_ids):
         return [self.__bot.get_channel(_) for _ in channel_ids]
 
-    def __fetch_commands(self, _documents):    # mode = "command"
+    def __fetch_commands(self, _documents):
         for command_name, channel_ids in _documents.items():
-            command = self.get("command", command_name)
+            command = self.get("commands", command_name)
             try:
                 command.disabled_channels.update(self.__get_channels(channel_ids))
             except AttributeError:
@@ -25,13 +26,21 @@ class DisabledFunctions(object):
             # Rather than dynamically patching, make commands use custom cls with disabled_channels
             # as one of its attributes and append channels to it.
 
-    def __fetch_plugins(self, _document):    # mode = "cog"
+    def __fetch_plugins(self, _document):
         for plugin_name, channel_ids in _document.items():
-            plugin = self.get("cog", plugin_name)
+            plugin = self.get("plugins", plugin_name)
             try:
                 plugin.disabled_channels.update(self.__get_channels(channel_ids))
             except AttributeError:
                 pass    # Cog is inescapable.
+
+    def __fetch_galaxies(self, _document):
+        for galaxy_name, channel_ids in _document.items():
+            galaxy = self.get("galaxies", galaxy_name)
+            try:
+                galaxy.disabled_channels.update(self.__get_channels(channel_ids))
+            except AttributeError:
+                pass    # Galaxy is inescapable.
 
     def get(self, mode, name):
         if mode == "commands":
@@ -39,7 +48,7 @@ class DisabledFunctions(object):
         elif mode == "plugins":
             return self.__bot.get_cog(name)
         elif mode == "galaxies":
-            return    # TODO: Implement get_galaxy method.
+            return self.__bot.get_galaxy(name)
         else:
             raise NameError
 
