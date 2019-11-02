7cccd8ca101280fb58e73d628a9152e32d591a62
thecosmos <deepakrajko14@gmail.com>
2019-11-03 00:02:05 +0530

    Cleaner cog - Actually implement commands.

--- cosmos/galaxies/guild/permissions/functions.py
+++ cosmos/galaxies/guild/permissions/functions.py
@@ -1,3 +1,6 @@
+import typing
+
+import discord
 from discord.ext import commands
 
 from ..settings.base import Settings
@@ -8,17 +11,13 @@ class CommandConverter(commands.Converter):
     async def convert(self, ctx, argument):
         if not (command := ctx.bot.get_command(argument.lower())):
             raise commands.BadArgument
-        if not hasattr(command, "disabled_channels"):
-            raise commands.BadArgument
         return command
 
 
 class PluginConverter(commands.Converter):
 
     async def convert(self, ctx, argument):
-        if not (plugin := ctx.bot.get_cog(argument.lower().title().replace(" ", str()))):
-            raise commands.BadArgument
-        if not hasattr(plugin, "disabled_channels"):
+        if not (plugin := ctx.bot.get_cog(argument)):
             raise commands.BadArgument
         return plugin
 
@@ -28,8 +27,6 @@ class GalaxyConverter(commands.Converter):
     async def convert(self, ctx, argument):
         if not (galaxy := ctx.bot.get_galaxy(argument.lower())):
             raise commands.BadArgument
-        if not hasattr(galaxy, "disabled_channels"):
-            raise commands.BadArgument
         return galaxy
 
 
@@ -38,33 +35,21 @@ class FunctionsPermissions(Settings):
     # TODO: Implement menu.
 
     @Settings.group(name="disable")
-    async def disable(self, ctx):
-        pass
-
-    @disable.command(name="command", aliases=["cmd"])
-    async def disable_command(self, ctx, command: CommandConverter):
-        pass
-
-    @disable.command(name="plugin", aliases=["cog"])
-    async def disable_plugin(self, ctx, *, plugin: PluginConverter):
-        pass
-
-    @disable.command(name="galaxy", aliases=["module"])
-    async def disable_galaxy(self, ctx, galaxy: GalaxyConverter):
-        pass
+    async def disable(self, ctx, function: typing.Union[CommandConverter, PluginConverter, GalaxyConverter],
+                      *channels: discord.TextChannel):
+        channels = channels or [ctx.channel]
+        await ctx.guild_profile.permissions.disable(function, channels)
+        # noinspection PyUnresolvedReferences
+        await ctx.send_line(f"✅    {function.name} has been disabled in specified channels.")
 
     @Settings.group(name="enable")
-    async def enable(self, ctx):
-        pass
+    async def enable(self, ctx, function: typing.Union[CommandConverter, PluginConverter, GalaxyConverter],
+                     *channels: discord.TextChannel):
+        channels = channels or [ctx.channel]
+        await ctx.guild_profile.permissions.enable(function, channels)
+        # noinspection PyUnresolvedReferences
+        await ctx.send_line(f"✅    {function.name} has been enabled back in specified channels.")
 
-    @enable.command(name="command", aliases=["cmd"])
-    async def enable_command(self, ctx, command: CommandConverter):
-        pass
-
-    @enable.command(name="plugin", aliases=["cog"])
-    async def enable_plugin(self, ctx, plugin: PluginConverter):
-        pass
-
-    @enable.command(name="galaxy", aliases=["module"])
-    async def enable_galaxy(self, ctx, galaxy: GalaxyConverter):
-        pass
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error.original, AttributeError):
+            await ctx.send_line(f"❌    You cannot disable that function.")
