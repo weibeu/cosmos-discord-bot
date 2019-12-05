dbfbcc8c126aa1ed81e5b3ee65200f8830643e05
thecosmos <deepakrajko14@gmail.com>
2019-12-05 18:32:25 +0530

    Implement Levels low level

--- cosmos/galaxies/profile/models/profiles/guild/level.py
+++ cosmos/galaxies/profile/models/profiles/guild/level.py
@@ -1,5 +1,3 @@
-import math
-
 from .base import GuildMemberProfileBase
 from abc import ABC, abstractmethod
 
@@ -7,10 +5,14 @@ from abc import ABC, abstractmethod
 class UserLevel(GuildMemberProfileBase, ABC):
 
     K = 5777
+    LEVELS_XP = [5 * (i ** 2) + 50 * i + 100 for i in range(200)]
 
-    def __init__(self, _level: int):
-        self._level = _level
-        self._xp_level = 0
+    def get_level(self, xp):
+        level = 0
+        while xp >= self.LEVELS_XP[level]:
+            xp -= self.LEVELS_XP[level]
+            level += 1
+        return level
 
     @property
     @abstractmethod
@@ -22,23 +24,27 @@ class UserLevel(GuildMemberProfileBase, ABC):
     def voice_xp(self):
         raise NotImplementedError
 
-    @property
-    def xp_level(self):
-        return self._xp_level + math.log(self._level + math.e)*self.K
+    # @property
+    # def xp_level(self):
+    #     return self._xp_level + math.log(self._level + math.e)*self.K
 
-    @property
-    def delta_xp(self):
-        return int(self.xp_level - self.xp)
-
-    def from_delta_xp(self):
-        while self.xp >= self.xp_level:    # TODO: Remove loop.
-            self._xp_level += self.xp_level
-            self._level += 1    # Don't really need self._level -= 1 'cause user will never loose xp.
-        return self._level
+    # @property
+    # def delta_xp(self):
+    #     return int(self.xp_level - self.xp)
+    #
+    # def from_delta_xp(self):
+    #     while self.xp >= self.xp_level:    # TODO: Remove loop.
+    #         self._xp_level += self.xp_level
+    #         self._level += 1    # Don't really need self._level -= 1 'cause user will never loose xp.
+    #     return self._level
 
     @property
     def level(self):
-        return self.from_delta_xp()
+        return self.get_level(self.xp)
+
+    @property
+    def voice_level(self):
+        return self.get_level(self.voice_xp)
 
     async def level_up_callback(self):
         guild_profile = await self.fetch_guild_profile()
