ed32540e565104381aab25548b71fa40d6639e42
thecosmos <deepakrajko14@gmail.com>
2019-12-07 15:55:13 +0530

    Adjust for voice levels

--- cosmos/galaxies/guild/levels/levels.py
+++ cosmos/galaxies/guild/levels/levels.py
@@ -31,8 +31,8 @@ class Levels(GuildBaseCog):
         return f"Level {entry.level}", value
 
     @levels.group(name="reward", aliases=["rewards"], invoke_without_command=True)
-    async def rewards(self, ctx, level: int = None):
-        rewards = ctx.guild_profile.levels.rewards
+    async def rewards(self, ctx, channel: typing.Optional[str] = "text", level: int = None):
+        rewards = ctx.guild_profile.levels.get_rewards(channel)
         if not rewards:
             return await ctx.send_line(f"❌    There are no level rewards set in this server yet.")
         if not level:
@@ -40,33 +40,34 @@ class Levels(GuildBaseCog):
                 sorted(
                     rewards.values(), key=lambda reward: reward.level
                 ), show_author=False, entry_parser=self.__rewards_parser, inline=False)
-            paginator.embed.set_author(name="Level Rewards", icon_url=ctx.guild.icon_url)
+            paginator.embed.set_author(name=f"Level {channel} Rewards".title(), icon_url=ctx.guild.icon_url)
             return await paginator.paginate()
         _reward = rewards.get(level)
         if not _reward:
             return await ctx.send_line(f"❌    There are no rewards assigned for level {level}.")
         embed = self.bot.theme.embeds.primary()
-        embed.set_author(name=f"Rewards for Level {level}", icon_url=ctx.guild.icon_url)
+        embed.set_author(name=f"Rewards for {channel} Level {level}", icon_url=ctx.guild.icon_url)
         embed.add_field(name="Roles", value=" ".join([f"<@&{role_id}>" for role_id in _reward.roles]))
         embed.add_field(name="Points", value=_reward.points)
         await ctx.send(embed=embed)
 
     @rewards.command(name="set")
     @has_permissions(administrator=True)
-    async def set_rewards(
-            self, ctx, level: int, points: typing.Optional[int] = 0, *, roles: converters.RoleConvertor()):
+    async def set_rewards(self, ctx, level: int, channel: typing.Optional[str] = "text",
+                          points: typing.Optional[int] = 0, *, roles: converters.RoleConvertor()):
         embed = self.bot.theme.embeds.primary()
-        embed.set_author(name=f"Are you sure to set following rewards for Level {level}?", icon_url=ctx.guild.icon_url)
+        embed.set_author(name=f"Are you sure to set following rewards for"
+                              f"{channel.title()} Level {level}?", icon_url=ctx.guild.icon_url)
         embed.add_field(name="Roles", value=" ".join([role.mention for role in roles]))
         embed.add_field(name="Points", value=points)
         if await ctx.confirm(await ctx.send(embed=embed)):
-            await ctx.guild_profile.levels.set_rewards(level, [role.id for role in roles], points)
-            await ctx.send_line(f"✅    Rewards for Level {level} has been set.")
+            await ctx.guild_profile.levels.set_rewards(level, [role.id for role in roles], points, channel=channel)
+            await ctx.send_line(f"✅    Rewards for {channel.title()} Level {level} has been set.")
 
     @rewards.command(name="remove", aliases=["delete"])
     @has_permissions(administrator=True)
-    async def remove_rewards(self, ctx, level: int):
+    async def remove_rewards(self, ctx, level: int, channel="text"):
         if not await ctx.confirm():
             return
-        await ctx.guild_profile.levels.remove_rewards(level)
+        await ctx.guild_profile.levels.remove_rewards(level, channel=channel)
         await ctx.send_line(f"✅    Rewards for Level {level} has been removed.")
