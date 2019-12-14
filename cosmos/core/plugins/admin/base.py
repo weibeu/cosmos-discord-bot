973eab6e3bb0cbfd469e001ae274b2959c7ee07f
thecosmos <deepakrajko14@gmail.com>
2019-12-15 01:02:25 +0530

    Pass the plugin object to base

--- cosmos/core/plugins/admin/base.py
+++ cosmos/core/plugins/admin/base.py
@@ -3,6 +3,10 @@ from ...functions import Cog
 
 class Admin(Cog):
 
+    def __init__(self, plugin):
+        super().__init__()
+        self.plugin = plugin
+
     async def cog_check(self, ctx):    # ! Never ever remove this method.
         await super().cog_check(ctx)
         return await self.bot.is_owner(ctx.author)
