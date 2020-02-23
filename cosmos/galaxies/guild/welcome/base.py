fd0d791776abbb16f52bbd6529588bacc6471e35
thecosmos <deepakrajko14@gmail.com>
2020-02-24 01:21:39 +0530

    Add Member class for message template

--- cosmos/galaxies/guild/welcome/base.py
+++ cosmos/galaxies/guild/welcome/base.py
@@ -7,3 +7,13 @@ class WelcomeBase(Settings):
     async def welcome(self, ctx):
         """Manage different welcome settings of your server."""
         pass
+
+
+class MessageTemplateMember(object):
+
+    def __init__(self, author):
+        self.id = author.id
+        self.username = str(author)
+        self.name = author.name
+        self.discriminator = author.discriminator
+        self.mention = author.mention
