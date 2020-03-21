4331a17894765de4034f65e7fb7c28fbd6fcdd18
thecosmos <deepakrajko14@gmail.com>
2020-03-21 20:54:03 +0530

    Handle GuildNotPrime exception

--- cosmos/core/plugins/error_handler/command.py
+++ cosmos/core/plugins/error_handler/command.py
@@ -2,6 +2,7 @@ import sys
 import traceback
 
 from ...functions import Cog
+from cosmos import exceptions
 
 
 class CommandErrorHandler(Cog):
@@ -12,6 +13,14 @@ class CommandErrorHandler(Cog):
 
     @Cog.listener()
     async def on_command_error(self, ctx, error):
+        # Do the basic logging first.
         self.bot.eh.sentry.capture_exception(error)
         self.bot.log.error(f"Ignoring exception in command {ctx.command}")
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+        if isinstance(error, exceptions.GuildNotPrime):
+            # Tried to use prime function in non-prime guild.
+            embed = ctx.embeds.primary()
+            embed.set_author(name=f"🔒    Click here to get prime and unlock all features including this."
+                                  f"", url="https://www.patreon.com/__thecosmos")
+            await ctx.send(embed=embed)
