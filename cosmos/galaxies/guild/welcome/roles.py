e1846d979bf1b305e7508a95e64e3a0968d19af4
thecosmos <deepakrajko14@gmail.com>
2020-03-25 20:27:05 +0530

    Implement actual welcome role command

--- cosmos/galaxies/guild/welcome/roles.py
+++ cosmos/galaxies/guild/welcome/roles.py
@@ -15,6 +15,13 @@ class WelcomeRoles(WelcomeBase):
     @WelcomeBase.welcome.group(name="roles", aliases=["role"])
     async def welcome_roles(self, ctx):
         """Displays the list of roles being assigned to every new members joining the server."""
+        if not ctx.guild_profile.welcome_roles:
+            return await ctx.send_line(f"❌    There are no roles assigned for welcome roles.")
+        embed = ctx.embed_line("Welcome Roles", ctx.author.avatar_url)
+        role_mentions = [f"{ctx.emotes.misc.next} {role.mention}" for role in ctx.guild_profile.welcome_roles]
+        embed.description = "\n".join(role_mentions) + "\n​"
+        embed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url)
+        await ctx.send(embed=embed)
 
     @welcome_roles.command(name="set", aliases=["setup", "add"])
     async def set_welcome_roles(self, ctx, *roles: discord.Role):
