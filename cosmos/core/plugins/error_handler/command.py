c9e8197e521b1b4a750025b7f5e149c24b155a58
thecosmos <deepakrajko14@gmail.com>
2020-04-03 11:34:43 +0530

    Use sentry known attributes

--- cosmos/core/plugins/error_handler/command.py
+++ cosmos/core/plugins/error_handler/command.py
@@ -45,7 +45,7 @@ class CommandErrorHandler(Cog):
 
         else:
             with configure_scope() as scope:
-                scope.user = {"name": str(ctx.author), "user_id": ctx.author.id}
+                scope.user = {"username": str(ctx.author), "id": ctx.author.id}
             self.bot.eh.sentry.capture_exception(error)
             self.bot.log.error(f"Ignoring exception in command {ctx.command}")
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
