f1babd22fd93df1a4ee9a97224f7d76e06d3aa6f
thecosmos <deepakrajko14@gmail.com>
2020-04-16 23:12:50 +0530

    Ignore when guild is None

--- cosmos/galaxies/profile/models/profiles/guild/guild_profile.py
+++ cosmos/galaxies/profile/models/profiles/guild/guild_profile.py
@@ -30,11 +30,14 @@ class GuildMemberProfile(GuildPoints, MemberExperience):
 
     def to_update_document(self):
         self.cache_voice_xp()
-        return {
-            f"{self.guild_filter}.stats.xp.chat": self.xp,
-            f"{self.guild_filter}.stats.xp.voice": self._voice_xp,
-            f"{self.guild_filter}.points.points": self.points,
-        }
+        try:
+            return {
+                f"{self.guild_filter}.stats.xp.chat": self.xp,
+                f"{self.guild_filter}.stats.xp.voice": self._voice_xp,
+                f"{self.guild_filter}.points.points": self.points,
+            }
+        except AttributeError:
+            pass
 
     async def log_moderation(self, _id):
         self.moderation_logs.append(_id)
