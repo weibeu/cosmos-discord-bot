2c4f534ec3af105656cabe280cef594c4a885a8c
thecosmos <deepakrajko14@gmail.com>
2020-04-23 22:17:21 +0530

    Ignore bots who wanna create their profile

--- cosmos/core/plugins/error_handler/_bot.py
+++ cosmos/core/plugins/error_handler/_bot.py
@@ -1,4 +1,8 @@
+import sys
+import traceback
+
 from ...functions import Cog
+from cosmos import exceptions
 
 
 class BotErrorHandler(Cog):
@@ -6,3 +10,13 @@ class BotErrorHandler(Cog):
     def __init__(self, plugin):
         super().__init__()
         self.plugin = plugin
+
+    @Cog.listener()
+    async def on_event_error(self, event, info):
+        _type, _value, _traceback = info
+        if isinstance(_value, exceptions.UserIsBotError):
+            pass
+
+        else:
+            self.bot.log.debug(f"Ignoring exception in {event}.")
+            traceback.print_exception(type(_value), _value, _value.__traceback__, file=sys.stderr)
