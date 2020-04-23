c9c2c38958ab4029db393cfa20e38661235af964
thecosmos <deepakrajko14@gmail.com>
2020-04-23 22:36:01 +0530

    Remove CosmosGuild and GuildMemberProfile from the internal cache when bot cannot see the guild

--- cosmos/core/plugins/error_handler/_bot.py
+++ cosmos/core/plugins/error_handler/_bot.py
@@ -17,6 +17,14 @@ class BotErrorHandler(Cog):
         if isinstance(_value, exceptions.UserIsBotError):
             pass    # TODO: Find all on_message's and return if bot if they using get_profile method. Don't flood here.
 
+        elif isinstance(_value, exceptions.GuildNotFoundError):
+            # Remove the guild from the internal cache. Including the GuildMemberProfile.
+            if _value.guild_id:
+                if _value.member_id:
+                    if profile := self.bot.profile_cache.lfu.get(_value.member_id):
+                        profile.guild_profiles.pop(_value.guild_id, None)
+                self.bot.guild_cache.lru.pop(_value.guild_id, None)
+
         else:
             self.bot.log.debug(f"Ignoring exception in {event}.")
             traceback.print_exception(type(_value), _value, _value.__traceback__, file=sys.stderr)
