6f8ad1d6d670f046f1d2b2cc25f91f4eec811870
thecosmos <deepakrajko14@gmail.com>
2020-04-24 23:01:29 +0530

    Add invite URL

--- cosmos/core/plugins/self/information.py
+++ cosmos/core/plugins/self/information.py
@@ -12,3 +12,9 @@ class CosmosInformation(Cog):
         """Love Cosmos? Help others find it by voting Cosmos on top.gg bot page <3."""
         res = "Click here to vote Cosmos on top.gg <3."
         await ctx.send_line(res, icon_url=self.bot.theme.images.heart, author_url=self.bot.configs.info.top_vote_page)
+
+    @Cog.command(name="invite")
+    async def invite_cosmos(self, ctx):
+        """Invite Cosmos to any of the server you manage."""
+        res = "Click here to invite Cosmos Bot to your server."
+        await ctx.send_line(res, icon_url=self.bot.theme.images.invite, author_url=self.bot.configs.discord.invite_url)
