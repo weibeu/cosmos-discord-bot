c655dc5c52662a5ab4558117bbff11db2c6eaf91
thecosmos <deepakrajko14@gmail.com>
2020-04-24 21:52:58 +0530

    Add author_url kwarg

--- cosmos/core/functions/theme/models/embeds/one_line.py
+++ cosmos/core/functions/theme/models/embeds/one_line.py
@@ -1,3 +1,5 @@
+from discord.embeds import EmptyEmbed
+
 from .primary import Primary
 
 
@@ -9,7 +11,7 @@ class OneLinePrimary(Primary):
         icon_url = kwargs.get("icon_url")
         if icon_url:
             super().__init__(**kwargs)
-            self.set_author(name=content, icon_url=icon_url)
+            self.set_author(name=content, icon_url=icon_url, url=kwargs.get("author_url") or EmptyEmbed)
         else:
             super().__init__(title=content, **kwargs)
 
@@ -23,8 +25,8 @@ class OneLine(object):
     def __init__(self, bot):
         self.bot = bot
 
-    def primary(self, content: str, icon_url=None, **kwargs):
-        return OneLinePrimary(self.bot, content, icon_url=icon_url, **kwargs)
+    def primary(self, content: str, icon_url=None, author_url=None, **kwargs):
+        return OneLinePrimary(self.bot, content, icon_url=icon_url, author_url=author_url, **kwargs)
 
     def discord(self, content: str, icon_url=None, **kwargs):
         discord_color = self.bot.configs.color_scheme.discord
