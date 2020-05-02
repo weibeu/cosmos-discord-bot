f6b165b8389e896078c2fa8fee6c01fa012ca187
thecosmos <deepakrajko14@gmail.com>
2020-05-02 12:27:00 +0530

    Use default GuildException base class with default handled attribute set to True

--- cosmos/galaxies/guild/_models/exceptions.py
+++ cosmos/galaxies/guild/_models/exceptions.py
@@ -1,11 +1,19 @@
 from discord.ext.commands import CommandError
 
 
-class NotEnoughPointsError(CommandError):
+class GuildExceptions(CommandError):
+
+    handled = True
+
+    def __init__(self, handled=None):
+        self.handled = handled or self.handled
+
+
+class NotEnoughPointsError(GuildExceptions):
 
     pass
 
 
-class RoleNotFoundError(CommandError):
+class RoleNotFoundError(GuildExceptions):
 
     pass
