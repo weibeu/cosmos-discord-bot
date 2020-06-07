6665a32d4da2c0e88812ebd31efcb5cdbbd2b716
thec0sm0s <deepakrajko14@gmail.com>
2020-06-07 22:02:07 +0530

    Use modular approach to display leaderboards

--- cosmos/galaxies/profile/leaderboards.py
+++ cosmos/galaxies/profile/leaderboards.py
@@ -1,6 +1,21 @@
+from discord.ext import commands
+
 import discord
 
 from .. import Cog
+
+
+class StatsConverter(commands.Converter):
+
+    STATS = [
+        "text", "chat", "voice",
+    ]
+
+    async def convert(self, ctx, argument):
+        argument = argument.lower()
+        if argument not in self.STATS:
+            raise commands.BadArgument
+        return argument
 
 
 class NotFoundUser(discord.Object):
@@ -22,12 +37,12 @@ class Leaderboards(Cog):
         super().__init__()
         self.plugin = plugin
 
-    async def fetch_top(self, _filter, limit=111):
+    async def fetch_top(self, filter_, limit=111):
         pipeline = [
-            {"$project": {"_id": False, "user_id": True, _filter: True}},
-            {"$match": {_filter: {"$exists": True}}},
+            {"$project": {"_id": False, "user_id": True, filter_: True}},
+            {"$match": {filter_: {"$exists": True}}},
             {"$group": {
-                "_id": "$user_id", "attribute": {"$max": f"${_filter}"}
+                "_id": "$user_id", "attribute": {"$max": f"${filter_}"}
             }},
             {"$sort": {"attribute": -1}}, {"$limit": limit}
         ]
@@ -76,29 +91,33 @@ class Leaderboards(Cog):
         key, value = await self.__entry_parser(*args, **kwargs)
         return key, f"`TOTAL GLOBAL XP:` {value}"
 
-    @Cog.group(name="leaderboards", aliases=["leaderboard", "lb"], invoke_without_command=True, inescapable=False)
-    async def leaderboards(self, ctx):
-        """Displays top members with maximum chat experience points."""
-        profile = await ctx.fetch_member_profile()
-        _filter = f"{profile.guild_filter}.stats.xp.chat"
-        entries = await self.__filter(ctx.guild.get_member, ctx.guild.fetch_member, await self.fetch_top(_filter))
-        paginator = ctx.get_field_paginator(entries, entry_parser=self.__xp_entry_parser, inline=False, per_page=10)
-        paginator.embed.set_author(
-            name=f"{ctx.guild.name} Chat XP Leaderboards".upper(), icon_url=self.bot.theme.images.leaderboard)
-        paginator.embed.set_thumbnail(url=ctx.guild.icon_url)
-        paginator.embed.description = f"```css\nDisplaying Leaderboards of top members with highest chat XP in " \
-                                      f"{ctx.guild.name}.```"
+    async def __show_leaderboards(self, ctx, entries, name, parser, global_=False):
+        paginator = ctx.get_field_paginator(entries, entry_parser=parser, inline=False, per_page=9)
+        icon = self.bot.theme.images.physics if global_ else ctx.guild.icon_url
+        paginator.embed.set_author(name=name, icon_url=icon)
         await paginator.paginate()
 
+    @Cog.group(name="leaderboards", aliases=["leaderboard", "lb"], invoke_without_command=True, inescapable=False)
+    async def leaderboards(self, ctx, stats: StatsConverter = "chat"):
+        """Displays top members with maximum chat experience points."""
+        profile = await ctx.fetch_member_profile()
+
+        if stats in ("chat", "text"):
+            filter_ = f"{profile.guild_filter}.stats.xp.chat"
+            name = f"{ctx.guild.name} Chat XP Leaderboards".upper()
+        else:
+            raise ValueError
+        entries = await self.__filter(ctx.guild.get_member, ctx.guild.fetch_member, await self.fetch_top(filter_))
+        await self.__show_leaderboards(ctx, entries, name, self.__xp_entry_parser)
+
     @leaderboards.command(name="global", aliases=["cosmos", "globals"])
-    async def global_leaderboards(self, ctx):
+    async def global_leaderboards(self, ctx, stats: StatsConverter = "chat"):
         """Displays top users with maximum chat experience earned globally across all servers."""
-        _filter = "stats.xp.chat"
-        entries = await self.__filter(self.bot.get_user, self.bot.fetch_user, await self.fetch_top(_filter))
-        paginator = ctx.get_field_paginator(
-            entries, entry_parser=self.__global_xp_entry_parser, inline=False, per_page=10)
-        paginator.embed.set_author(
-            name=f"Global Chat XP Leaderboards".upper(), icon_url=self.bot.theme.images.leaderboard)
-        paginator.embed.set_thumbnail(url=self.bot.user.avatar_url)
-        paginator.embed.description = f"```css\nDisplaying Leaderboards of top users with highest chat XP in Cosmos.```"
-        await paginator.paginate()
+
+        if stats in ("chat", "text"):
+            filter_ = "stats.xp.chat"
+            name = f"Cosmos Universe Chat XP Leaderboards".upper()
+        else:
+            raise ValueError
+        entries = await self.__filter(self.bot.get_user, self.bot.fetch_user, await self.fetch_top(filter_))
+        await self.__show_leaderboards(ctx, entries, name, self.__global_xp_entry_parser, True)
