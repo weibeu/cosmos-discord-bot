230ad1a9c10b6d42879bb6dee0bc2f10e902ffbc
thec0sm0s <deepakrajko14@gmail.com>
2020-06-14 23:05:19 +0530

    Add support for .mp4 videos

--- cosmos/galaxies/tools/imgur.py
+++ cosmos/galaxies/tools/imgur.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 
+import mimetypes
 from .. import Cog
 from ...core.utilities import api
 
@@ -18,7 +19,12 @@ class Imgur(Cog):
             raise commands.BadArgument
         url = url or ctx.message.attachments[0].url
         try:
-            image = await self.bot.utilities.imgur.upload(image=url)
+            type_ = mimetypes.guess_type(url)
+            if type_[0] in ("video/mp4", ):
+                media = dict(video=url)
+            else:
+                media = dict(image=url)
+            image = await self.bot.utilities.imgur.upload(**media)
         except api.ImgurHTTPException:
             return await ctx.send_line("❌    Failed to upload provided image or URL to imgur.")
         return await ctx.send_line(
