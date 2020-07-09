21aa9470c2c69144f28c8bad6f351f256a8e2916
thec0sm0s <deepakrajko14@gmail.com>
2020-07-09 22:47:25 +0530

    Include __eq__

--- cosmos/core/functions/scheduler/task.py
+++ cosmos/core/functions/scheduler/task.py
@@ -17,6 +17,9 @@ class ScheduledTask(object):
         if self.invoke_at <= self.created_at:
             raise ValueError("The task must be scheduled to future.")
 
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.id == other.id
+
     def __call__(self, *args, **kwargs):
         if self.callback.startswith("on_"):
             return self.scheduler.bot.dispatch(self.callback, self, *args, **kwargs)
