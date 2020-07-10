58b61e5be0ca1a9ab775dc4a4653e38d9a77ee7e
thec0sm0s <deepakrajko14@gmail.com>
2020-07-11 00:34:50 +0530

    Initialize schedulers before intializing plugins

--- cosmos/core/tasks.py
+++ cosmos/core/tasks.py
@@ -48,10 +48,10 @@ class InitialTasks(commands.Bot):
         self._init_database()
         self._init_caches()
         self._init_emotes()
+        self._init_scheduler()
         self._init_plugins()
         self._init_theme()
         self._init_image_processor()
-        self._init_scheduler()
         self._init_misc_tasks()
 
     @Time.calc_time
