09697ac1b9d85341aebc12bb16f07fed9754f696
thec0sm0s <deepakrajko14@gmail.com>
2020-09-07 00:12:12 +0530

    Do not lazily fetch the prime owner. Rather block the call till we are done fetching owner and then return

--- cosmos/galaxies/guild/_models/prime.py
+++ cosmos/galaxies/guild/_models/prime.py
@@ -29,9 +29,8 @@ class CosmosGuildPrime(CosmosGuildBase, ABC):
 
     def __init__(self, **kwargs):
         self.prime_owner = None
-        self.plugin.bot.loop.create_task(self.__fetch_prime_owner())
 
-    async def __fetch_prime_owner(self):
+    async def fetch_prime_owner(self):
         if document := await self.plugin.bot.get_galaxy("PROFILE").collection.find_one(
                 {"prime.guild": self.id}, projection={"user_id": True}
         ):
