65da22b0ffcb9133da9a2d4ac02236812b8320bc
thec0sm0s <deepakrajko14@gmail.com>
2020-09-06 21:06:34 +0530

    Add basic PatreonUser model

--- cosmos/core/functions/server/views/patreon.py
+++ cosmos/core/functions/server/views/patreon.py
@@ -19,6 +19,42 @@ from aiohttp import web
 import hmac
 
 
+class PatreonUser(object):
+
+    @staticmethod
+    def __get_objects(included):
+        return {d["type"]: d["attributes"] for d in included if d.get("type")}
+
+    @property
+    def discord_id(self):
+        try:
+            return int(self._objects["user"]["social_connections"]["discord"]["user_id"])
+        except KeyError:
+            pass
+
+    @property
+    def pledge_amount(self):
+        try:
+            cents = self._objects["tier"]["amount_cents"]
+        except KeyError:
+            cents = self._pledge_amount
+        return cents // 100
+
+    @property
+    def tier(self):
+        return [t for t in sorted(self.__handler.bot.PrimeTier) if self.pledge_amount >= t.value][-1]
+
+    def __init__(self, handler, data):
+        self.__handler = handler
+        attrs = data["data"]["attributes"]
+        self._objects = self.__get_objects(data.get("included") or [])
+        self.is_former = attrs.get("patron_status") == "former_patron"
+        self._pledge_amount = attrs.get("will_pay_amount_cents") or attrs.get("currently_entitled_amount_cents")
+
+    async def fetch_cosmos_user_profile(self):
+        return await self.__handler.fetch_cosmos_user_profile(self.discord_id)
+
+
 class PatreonHook(base.BaseView):
 
     ROUTE = "/patreon/"
@@ -36,7 +72,9 @@ class PatreonHook(base.BaseView):
     async def post(self):
         if not await self.__authorize():
             raise web.HTTPForbidden
-        trigger = self.request.headers["X-Patreon-Event"].replace(":", "_")
         json = await self.request.json()
+        self.bot.log.info(f"Received [{self.NAME}] payload: {json}.")
+        trigger = self.request.headers["X-Patreon-Event"].replace(":", "_")
         event = self.DISPATCH_EVENT.format(trigger=trigger)
+        self.bot.dispatch(event, PatreonUser(self, json))
         return web.Response()
