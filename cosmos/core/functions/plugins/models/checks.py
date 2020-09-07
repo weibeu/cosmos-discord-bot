b7e6118b3a084dd5141fd731993a340069b1e4b2
thec0sm0s <deepakrajko14@gmail.com>
2020-09-07 14:20:26 +0530

    Add prime_user commands check

--- cosmos/core/functions/plugins/models/checks.py
+++ cosmos/core/functions/plugins/models/checks.py
@@ -18,10 +18,20 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from discord.ext import commands
 
+from ...exceptions import UserNotPrime
 from ...exceptions import GuildNotPrime
 
 
 class CosmosChecks(object):
+
+    @classmethod
+    def prime_user(cls):
+        async def predicate(ctx):
+            if not (await ctx.bot.profile_cache.get_profile(ctx.author.id)).is_prime:
+                raise UserNotPrime
+            return True
+
+        return commands.check(predicate)
 
     @classmethod
     def prime_guild(cls):
