50a3db13754896709b2f556514f2bb6aa3d56965
thec0sm0s <deepakrajko14@gmail.com>
2020-10-07 11:16:15 +0530

    Minor fixes

--- cosmos/galaxies/search/movies.py
+++ cosmos/galaxies/search/movies.py
@@ -16,6 +16,8 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from discord.embeds import EmptyEmbed
+
 from .. import Cog
 
 
@@ -28,18 +30,20 @@ class MoviesSearch(Cog):
 
     async def __display_movie(self, movie, channel):
         embed = self.bot.theme.embeds.primary()
-        embed.set_thumbnail(url=movie.poster)
+        embed.set_thumbnail(url=movie.poster or EmptyEmbed)
         embed.description = movie.overview
         embed.set_author(
-            name=movie.title, url=movie.homepage,
+            name=movie.title, url=movie.homepage or EmptyEmbed,
             icon_url=self.bot.theme.images.movie,
         )
         embed.set_footer(
             text=f"{movie.votes} Votes | {movie.status}",
             icon_url=self.bot.configs.tmdb.brand_icon_url
         )
-        embed.add_field(name=f"{self.bot.emotes.misc.director}    Director", value=movie.credits.director.name)
-        embed.add_field(name=f"{self.bot.emotes.misc.two_hearts}    Genres", value=", ".join(movie.genres))
+        if movie.credits.director:
+            embed.add_field(name=f"{self.bot.emotes.misc.director}    Director", value=movie.credits.director.name)
+        if movie.genres:
+            embed.add_field(name=f"{self.bot.emotes.misc.two_hearts}    Genres", value=", ".join(movie.genres))
         if movie.revenue:
             embed.add_field(
                 name=f"{self.bot.emotes.misc.cash}    Total Revenue",
@@ -47,10 +51,11 @@ class MoviesSearch(Cog):
             )
         if movie.release_date:
             embed.timestamp = movie.release_date
-        embed.add_field(
-            name=f"{self.bot.emotes.misc.documentary}    Productions",
-            value=" • ".join(movie.productions), inline=False
-        )
+        if movie.productions:
+            embed.add_field(
+                name=f"{self.bot.emotes.misc.documentary}    Productions",
+                value=" • ".join(movie.productions), inline=False
+            )
         return await channel.send(embed=embed)
 
     # @Cog.cooldown(1, 5, type=Cog.bucket_type.user)
