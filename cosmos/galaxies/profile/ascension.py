363fff5fd10f7d3c8602b24adba944da5b1d21b4
boy-in-blue <sanskarxcix@gmail.com>
2021-03-31 15:50:40 +0530

    ✔ fix vc xp

--- cosmos/galaxies/profile/ascension.py
+++ cosmos/galaxies/profile/ascension.py
@@ -26,6 +26,7 @@ class _Levels(Cog):
         super().__init__()
         self.plugin = plugin
         self.cache = self.plugin.cache
+        self.discord_guilds = {}
 
     def __is_ignored(self, message):
         if message.author.id == self.bot.user.id:
@@ -49,66 +50,179 @@ class _Levels(Cog):
         except AttributeError:
             return []
 
+    @staticmethod
+    def valid_members(members: list) -> list:
+        return [m for m in members if
+                not m.voice.self_deaf and
+                not m.voice.mute and
+                not m.voice.deaf and
+                not m.voice.self_mute and
+                not m.voice.afk]
+
     async def get_guild_profile(self, member_id, guild_id):
         profile = await self.cache.get_profile(member_id)
         return await profile.get_guild_profile(guild_id)
 
     @Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        if member.bot or after.afk:
+        if member.bot:
             return
 
         guild_profile = await self.get_guild_profile(member.id, member.guild.id)
 
-        if ((before.mute and not after.mute)
-                or (before.self_mute and not after.self_mute)
-                or (not before.channel and after.channel and not (after.mute or after.self_mute))):
+        # user leaving a channel
+        if not after.channel and before.channel:
+            if member.guild in self.discord_guilds:
+                if before.channel in self.discord_guilds[member.guild]:
+                    if member.id in self.discord_guilds[member.guild][before.channel]:
+                        self.discord_guilds[member.guild][before.channel].remove(member.id)
+                        guild_profile.close_voice_activity()
+                        guild_profile.profile.close_voice_activity()
+                    if len(members := self.get_vc_members(before.channel)) > 0:
+                        if len(valid_members := self.valid_members(members)) < 2:
+                            for m in valid_members:
+                                gp = await self.get_guild_profile(m.id, member.guild.id)
+                                gp.close_voice_activity()
+                                gp.profile.close_voice_activity()
+                    else:
+                        self.discord_guilds[member.guild].pop(before.channel)
+                        if len(self.discord_guilds[member.guild]) == 0:
+                            self.discord_guilds.pop(member.guild)
+        else:
+            pass
 
-            # if len(self.get_vc_members(after.channel)) <= 1:
-            #     return    # Wait for someone to join vc.
+        # user channel hopping / mute-unmute etc.
+        if before.channel and after.channel:
+            # destroy channel if noone is there
+            if len(self.get_vc_members(before.channel)) < 1:
+                self.discord_guilds[member.guild].pop(before.channel)
+            else:
+                pass
 
-            guild_profile.record_voice_activity()
-            guild_profile.profile.record_voice_activity()
+            # mute-unmute etc.
+            if before.channel == after.channel:
+                # if did mute-deaf, stop xp
+                if ((not before.mute and after.mute) or
+                    (not before.deaf and after.deaf) or
+                    (not before.self_mute and after.self_mute) or
+                        (not before.self_deaf and after.self_mute)):
+                    guild_profile.close_voice_activity()
+                    guild_profile.profile.close_voice_activity()
+                    # if old channel dont have enough members, stop their xp
+                    if len(valid_members := self.valid_members(self.get_vc_members(before.channel))) < 2:
+                        for m in valid_members:
+                            gp = await self.get_guild_profile(m.id, member.guild.id)
+                            gp.close_voice_activity()
+                            gp.profile.close_voice_activity()
+                    else:
+                        pass
 
-        if ((not before.mute and after.mute)
-                or (not before.self_mute and after.self_mute)
-                or (before.channel and not after.channel and not (before.mute or before.self_mute))):
-            guild_profile.close_voice_activity()
-            guild_profile.profile.close_voice_activity()
+                # if did unmute-undeaf
+                elif ((before.mute and not after.mute) or
+                      (before.deaf and not after.deaf) or
+                      (before.self_mute and not after.self_mute) or
+                        (before.self_deaf and not after.self_mute)):
+                    # if channel now has enough valid members, start their xp
+                    if len(valid_members := self.valid_members(self.get_vc_members(after.channel))) > 1:
+                        for m in valid_members:
+                            gp = await self.get_guild_profile(m.id, member.guild.id)
+                            gp.record_voice_activity()
+                            gp.profile.record_voice_activity()
+                    else:
+                        pass
+                else:
+                    pass
 
-        # if after.channel and not before.channel:
-        #     members = self.get_vc_members(before.channel)
-        #     if not len(members) == 1:
-        #         return
-        #     # Someone joined vc. Check and record voice activity for member who is waiting.
-        #     waiting_member = members[0]
-        #     waiting_member_vs = waiting_member.voice
-        #     if waiting_member_vs.self_mute or waiting_member_vs.mute:
-        #         return
-        #     waiting_profile = await self.get_guild_profile(waiting_member.id, waiting_member.guild.id)
-        #     waiting_profile.record_voice_activity()
-        #
-        # if before.channel and not after.channel:
-        #     members = self.get_vc_members(before.channel)
-        #     if not len(members) == 1:
-        #         return
-        #     # Someone left vc and left one member alone. Stop his voice activity.
-        #     waiting_member = members[0]
-        #     waiting_profile = await self.get_guild_profile(waiting_member.id, waiting_member.guild.id)
-        #     waiting_profile.close_voice_activity()
+            # if channel hop, close activity
+            elif before.channel != after.channel:
+                if member.guild in self.discord_guilds:
+                    if after.channel in self.discord_guilds[member.guild]:
+                        self.discord_guilds[member.guild][after.channel].append(member.id)
+                    elif after.channel not in self.discord_guilds[member.guild]:
+                        self.discord_guilds[member.guild][after.channel] = [member.id]
+                    else:
+                        pass
+                elif member.guild not in self.discord_guilds:
+                    self.discord_guilds[member.guild] = {}
+                    self.discord_guilds[member.guild][after.channel] = [member.id]
+                else:
+                    pass
 
-    @Cog.listener()
+                guild_profile.close_voice_activity()
+                guild_profile.profile.close_voice_activity()
+
+                # if new channel has enough members, start their xp
+                if len(members := self.get_vc_members(after.channel)) > 1:
+                    if len(valid_members := self.valid_members(members)) > 1:
+                        for m in valid_members:
+                            gp = await self.get_guild_profile(m.id, member.guild.id)
+                            gp.record_voice_activity()
+                            gp.profile.record_voice_activity()
+                    else:
+                        pass
+                else:
+                    pass
+
+                # if old channel lost enough members, close their xp
+                if len(members_remain := self.get_vc_members(before.channel)) < 2:
+                    valid_members = self.valid_members(members_remain)
+                    for m in valid_members:
+                        gp = await self.get_guild_profile(m.id, member.guild.id)
+                        gp.close_voice_activity()
+                        gp.profile.close_voice_activity()
+                else:
+                    pass
+
+        else:
+            pass
+
+        # user join a channel
+        if not before.channel and after.channel:
+            if member.guild in self.discord_guilds:
+                if after.channel in self.discord_guilds[member.guild]:
+                    self.discord_guilds[member.guild][after.channel].append(member.id)
+            elif member.guild not in self.discord_guilds:
+                self.discord_guilds[member.guild] = {}
+                self.discord_guilds[member.guild][after.channel] = [member.id]
+            else:
+                pass
+
+            # if enough members, start their xp
+            if len(members := self.get_vc_members(after.channel)) > 1:
+                if len(valid_members := self.valid_members(members)) > 1:
+                    for m in valid_members:
+                        gp = await self.get_guild_profile(m.id, member.guild.id)
+                        gp.record_voice_activity()
+                        gp.profile.record_voice_activity()
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+
+    @ Cog.listener()
     async def on_ready(self):
         for g in self.bot.guilds:
             for vc in g.voice_channels:
-                for user_id, vs in vc.voice_states.items():
-                    user = self.bot.get_user(user_id)
-                    if user.bot:
-                        continue
-                    if vs.self_mute or vs.mute:
-                        continue
-                    # if len(self.get_vc_members(vs.channel)):
-                    #     continue
-                    guild_profile = await self.get_guild_profile(user_id, g.id)
-                    guild_profile.record_voice_activity()
-                    guild_profile.profile.record_voice_activity()
+                if len(self.get_vc_members(vc)) > 0:
+                    if g not in self.discord_guilds:
+                        self.discord_guilds[g] = {}
+                        self.discord_guilds[g] = {vc: [m.id for m in self.get_vc_members(vc)]}
+                    else:
+                        self.discord_guilds[g][vc] = [m.id for m in self.get_vc_members(vc)]
+                else:
+                    pass
+
+        for dg in self.discord_guilds:
+            for vc_chan in self.discord_guilds[dg]:
+                if len(members := self.get_vc_members(vc_chan)) > 1:
+                    if len(valid_members := self.valid_members(members)) > 1:
+                        for m in valid_members:
+                            gp = await self.get_guild_profile(m.id, dg.id)
+                            gp.record_voice_activity()
+                            gp.profile.record_voice_activity()
+                    else:
+                        pass
+                else:
+                    pass
